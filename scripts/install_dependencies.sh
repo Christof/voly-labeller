db314d7030896af25cdb5fa013b8ca4d9ae6af9a
Christof Sirk <christof.sirk@gmail.com>
2015-03-15 15:23:26 +0100

    Add install script to install assimp.

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+#Terminate on error
+set -e
+
+USER_NAME=$USERNAME
+USER_HOME=/home/$USER_NAME
+USER_SOURCES=$USER_HOME/Documents/sources
+
+cd $USER_HOME
+
+mkdir -p USER_SOURCES
+
+sudo apt-get install libassimp-dev -y
