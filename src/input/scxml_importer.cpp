b85ec6b787206d31d49705c7b7bebd614b7a9ad3
Christof Sirk <christof.sirk@gmail.com>
2015-03-24 19:45:46 +0100

    Build state machine with states and transitions.

--- src/input/scxml_importer.cpp
+++ src/input/scxml_importer.cpp
@@ -2,6 +2,9 @@
 #include <QXmlStreamReader>
 #include <QFile>
 #include <QStateMachine>
+#include <QAbstractTransition>
+#include <QState>
+#include <QFinalState>
 #include <QEvent>
 #include <QKeySequence>
 #include <QKeyEventTransition>
@@ -35,71 +38,124 @@ uint toKey(QString const &str)
   return keyCode;
 }
 
-ScxmlImporter::ScxmlImporter(QUrl url)
+ScxmlImporter::ScxmlImporter(QUrl url, QObject *keyboardEventReceiver)
+  : keyboardEventReceiver(keyboardEventReceiver)
 {
   QFile file(url.toLocalFile());
-  std::cout << file.exists() << std::endl;
   file.open(QFile::OpenModeFlag::ReadOnly);
 
   stateMachine = std::shared_ptr<QStateMachine>(new QStateMachine());
+  connect(stateMachine.get(), &QStateMachine::finished, []
+          {
+    std::cout << "finished" << std::endl;
+  });
 
-  QXmlStreamReader reader(file.readAll());
-  State *state;
-  QString initialState;
-  while (!reader.atEnd() && !reader.hasError())
+  reader =
+      std::unique_ptr<QXmlStreamReader>(new QXmlStreamReader(file.readAll()));
+
+  while (!reader->atEnd() && !reader->hasError())
   {
-    QXmlStreamReader::TokenType token = reader.readNext();
+    QXmlStreamReader::TokenType token = reader->readNext();
     if (token == QXmlStreamReader::StartDocument)
-    {
       continue;
-    }
 
     if (token == QXmlStreamReader::StartElement)
-    {
-      if (reader.name() == "scxml")
-      {
-        initialState = reader.attributes().value("initialstate").toString();
-      }
-      if (reader.name() == "state")
-      {
-        auto stateName = reader.attributes().value("id").toString();
-        state = new State(stateName.toStdString());
-        stateMachine->addState(state);
-
-        if (stateName == initialState)
-          stateMachine->setInitialState(state);
-
-        std::cout << "state: " << state->name << std::endl;
-        // reader.readElementText(QXmlStreamReader::IncludeChildElements).toStdString()
-        // << std::endl;
-      }
-      if (reader.name() == "transition")
-      {
-        auto event = reader.attributes().value("event").toString();
-        auto target = reader.attributes().value("target").toString();
-        QString keyboardEvent = "KeyboardEvent";
-        if (event.startsWith(keyboardEvent))
-        {
-          QEvent::Type eventType = QEvent::KeyPress;
-          auto keyAsString = event.mid(event.lastIndexOf(".") + 1);
-          auto keyCode = toKey(keyAsString);
-          auto transition =
-              new QKeyEventTransition(nullptr, eventType, keyCode);
-          state->addTransition(transition);
-          std::cout << keyAsString.toStdString() << ": " << keyCode << "|"
-                    << std::to_string(Qt::Key_A) << std::endl;
-        }
-        std::cout << "transition: " << event.toStdString() << " | "
-                  << target.toStdString() << std::endl;
-      }
-    }
+      readElement();
   }
 
-  if (reader.hasError())
-    std::cerr << "Error while parsing: " << reader.errorString().toStdString()
+  qRegisterMetaType<QAbstractState *>("QAbstractState*");
+  for (auto transition : transitions)
+  {
+    auto targetStateName = transition->property("targetState").toString();
+    transition->setTargetState(states[targetStateName]);
+  }
+
+  if (reader->hasError())
+    std::cerr << "Error while parsing: " << reader->errorString().toStdString()
               << std::endl;
 }
 
 ScxmlImporter::~ScxmlImporter()
 {
+}
+
+void ScxmlImporter::readElement()
+{
+  if (reader->name() == "scxml")
+  {
+    initialState = attributeAsString("initialstate");
+  }
+  if (reader->name() == "state")
+  {
+    readState();
+  }
+  if (reader->name() == "transition")
+  {
+    readTransition();
+  }
+  if (reader->name() == "final")
+  {
+    auto stateName = attributeAsString("id");
+    auto finalState = new QFinalState();
+    finalState->setProperty("name", stateName);
+    stateMachine->addState(finalState);
+    states[stateName] = finalState;
+  }
+}
+
+void ScxmlImporter::readState()
+{
+  auto stateName = attributeAsString("id");
+  state = new QState();
+  state->setProperty("name", stateName);
+  stateMachine->addState(state);
+
+  if (stateName == initialState)
+    stateMachine->setInitialState(state);
+
+  auto currentState = state;
+  connect(state, &QState::entered, [currentState]()
+          {
+    std::cout << "entered: "
+              << currentState->property("name").toString().toStdString()
+              << std::endl;
+  });
+
+  std::cout << "state: " << state->property("name").toString().toStdString()
+            << std::endl;
+
+  states[stateName] = state;
+}
+
+void ScxmlImporter::readTransition()
+{
+  auto event = attributeAsString("event");
+  auto target = attributeAsString("target");
+  QString keyboardEvent = "KeyboardEvent";
+  if (event.startsWith(keyboardEvent))
+  {
+    QEvent::Type eventType = QEvent::KeyPress;
+    auto keyAsString = event.mid(event.lastIndexOf(".") + 1);
+    auto keyCode = toKey(keyAsString);
+    auto transition =
+        new QKeyEventTransition(keyboardEventReceiver, eventType, keyCode);
+    transition->setProperty("targetStae", target);
+    state->addTransition(transition);
+    std::cout << keyAsString.toStdString() << ": " << keyCode << "|"
+              << std::to_string(Qt::Key_A) << std::endl;
+
+    transitions.push_back(transition);
+  }
+  std::cout << "transition: " << event.toStdString() << " | "
+            << target.toStdString() << std::endl;
+}
+
+QString ScxmlImporter::attributeAsString(const char *name)
+{
+  return reader->attributes().value(name).toString();
+}
+
+std::shared_ptr<QStateMachine> ScxmlImporter::getStateMachine()
+{
+  return stateMachine;
 }
--- src/input/scxml_importer.h
+++ src/input/scxml_importer.h
@@ -3,43 +3,46 @@
 #define SRC_INPUT_SCXML_IMPORTER_H_
 
 #include <QUrl>
-#include <QState>
+#include <QString>
+#include <QObject>
 #include <memory>
+#include <map>
+#include <vector>
+
+class QStateMachine;
+class QAbstractState;
+class QAbstractTransition;
+class QXmlStreamReader;
+class QState;
 
 /**
  * \brief
  *
  *
  */
-class State : public QState
+class ScxmlImporter : public QObject
 {
+  Q_OBJECT
  public:
-  State(std::string name) : name(name)
-  {
-  }
-  virtual ~State()
-  {
-  }
-
-  std::string name;
-
- private:
-  /* data */
-};
-
-/**
- * \brief
- *
- *
- */
-class ScxmlImporter
-{
- public:
-  ScxmlImporter(QUrl url);
+  ScxmlImporter(QUrl url, QObject *keyboardEventReceiver);
   virtual ~ScxmlImporter();
 
+  std::shared_ptr<QStateMachine> getStateMachine();
+
  private:
+  QObject *keyboardEventReceiver;
+  QString initialState;
+  QState *state;
+  std::unique_ptr<QXmlStreamReader> reader;
   std::shared_ptr<QStateMachine> stateMachine;
+  std::map<QString, QAbstractState *> states;
+  std::vector<QAbstractTransition *> transitions;
+
+  void readElement();
+  void readState();
+  void readTransition();
+
+  QString attributeAsString(const char *name);
 };
 
 #endif  // SRC_INPUT_SCXML_IMPORTER_H_
