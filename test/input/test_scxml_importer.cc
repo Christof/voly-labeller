9f7369e6a9cdbba077a3e8724784df6a560b4f42
Christof Sirk <christof.sirk@gmail.com>
2015-03-24 19:46:05 +0100

    Test for now starts an application.

--- test/input/test_scxml_importer.cc
+++ test/input/test_scxml_importer.cc
@@ -1,8 +1,36 @@
 #include "../test.h"
 #include <QUrl>
+#include <QStateMachine>
+#include <QGuiApplication>
+#include <QWindow>
+#include <QKeyEvent>
+#include <thread>
+#include <chrono>
 #include "../../src/input/scxml_importer.h"
 
 TEST(Test_ScxmlImporter, foo)
 {
-  ScxmlImporter importer(QUrl::fromLocalFile("../config/simple_state.xml"));
+  int zero = 0;
+  QGuiApplication application(zero, static_cast<char **>(nullptr));
+  QWindow window;
+
+  ScxmlImporter importer(QUrl::fromLocalFile("../config/simple_state.xml"),
+                         &window);
+  auto stateMachine = importer.getStateMachine();
+  std::cout << "running:" << stateMachine->isRunning() << std::endl;
+
+  stateMachine->start();
+
+  window.show();
+
+  std::cout << "running:" << stateMachine->isRunning() << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+  std::cout << "error:" << stateMachine->errorString().toStdString() << std::endl;
+  std::cout << "running:" << stateMachine->isRunning() << std::endl;
+
+  stateMachine->postEvent(
+      new QKeyEvent(QEvent::KeyPress, Qt::Key_A, Qt::NoModifier));
+
+  application.exec();
 }
