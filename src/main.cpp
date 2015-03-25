003b1366e963c1abc4910a17764768d4267bcd32
Christof Sirk <christof.sirk@gmail.com>
2015-03-25 20:01:37 +0100

    Make window available in QML as window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <QtGui/QGuiApplication>
+#include <QQmlContext>
 #include <memory>
 #include "./window.h"
 #include "./demo_scene.h"
@@ -8,6 +9,7 @@ int main(int argc, char **argv)
   QGuiApplication application(argc, argv);
   auto scene = std::make_shared<DemoScene>();
   Window window(scene);
+  window.rootContext()->setContextProperty("window", &window);
   window.setSource(QUrl("qrc:ui.qml"));
 
   window.show();
