41190417b66f54b2ea75fe867a4ea5dd5bb30b01
Christof Sirk <christof.sirk@gmail.com>
2015-03-27 14:19:46 +0100

    Add window to test.

--- test/input/test_scxml_importer.cc
+++ test/input/test_scxml_importer.cc
@@ -2,31 +2,58 @@
 #include <QUrl>
 #include <QStateMachine>
 #include <QGuiApplication>
-#include <QWindow>
+#include <QtQuick/QQuickView>
 #include <QKeyEvent>
 #include <thread>
 #include <chrono>
 #include "../../src/input/scxml_importer.h"
 
+/**
+ * \brief
+ *
+ *
+ */
+class MyWindow : public QQuickView
+{
+ public:
+  MyWindow() : QQuickView(nullptr){};
+  virtual ~MyWindow() {
+  }
+
+ signals:
+  void keyPressed(QKeyEvent *e) {Q_UNUSED(e)}
+
+ protected:
+  inline void keyPressEvent(QKeyEvent * e) Q_DECL_OVERRIDE
+  {
+    std::cout << "key event: " << e->text().toStdString() << std::endl;
+    emit keyPressed(e);
+    //QQuickView::event(e);
+  }
+};
+
 TEST(Test_ScxmlImporter, foo)
 {
   int zero = 0;
   QGuiApplication application(zero, static_cast<char **>(nullptr));
-  QWindow window;
+  MyWindow window;
+  window.setSource(QUrl("qrc:ui.qml"));
 
+  window.show();
+
+  auto invokeManager = std::shared_ptr<InvokeManager>(new InvokeManager());
   ScxmlImporter importer(QUrl::fromLocalFile("../config/simple_state.xml"),
-                         &window);
+                         &window, invokeManager);
   auto stateMachine = importer.getStateMachine();
   std::cout << "running:" << stateMachine->isRunning() << std::endl;
 
   stateMachine->start();
 
-  window.show();
-
   std::cout << "running:" << stateMachine->isRunning() << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-  std::cout << "error:" << stateMachine->errorString().toStdString() << std::endl;
+  std::cout << "error:" << stateMachine->errorString().toStdString()
+            << std::endl;
   std::cout << "running:" << stateMachine->isRunning() << std::endl;
 
   stateMachine->postEvent(
@@ -34,3 +61,4 @@ TEST(Test_ScxmlImporter, foo)
 
   application.exec();
 }
+
