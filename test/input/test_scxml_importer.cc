656c66b03d9fabb4b06067267769425f2fbc50b6
Christof Sirk <christof.sirk@gmail.com>
2015-03-30 20:09:10 +0200

    First real unit test for the state machine.

--- test/input/test_scxml_importer.cc
+++ test/input/test_scxml_importer.cc
@@ -1,64 +1,41 @@
 #include "../test.h"
 #include <QUrl>
 #include <QStateMachine>
-#include <QGuiApplication>
+#include <QCoreApplication>
 #include <QtQuick/QQuickView>
 #include <QKeyEvent>
 #include <thread>
 #include <chrono>
 #include "../../src/input/scxml_importer.h"
 
-/**
- * \brief
- *
- *
- */
-class MyWindow : public QQuickView
-{
- public:
-  MyWindow() : QQuickView(nullptr){};
-  virtual ~MyWindow() {
-  }
-
- signals:
-  void keyPressed(QKeyEvent *e) {Q_UNUSED(e)}
-
- protected:
-  inline void keyPressEvent(QKeyEvent * e) Q_DECL_OVERRIDE
-  {
-    std::cout << "key event: " << e->text().toStdString() << std::endl;
-    emit keyPressed(e);
-    //QQuickView::event(e);
-  }
-};
-
-TEST(Test_ScxmlImporter, foo)
+TEST(Test_ScxmlImporter, SwitchFromInitialToFinalStateWithKeyPress)
 {
   int zero = 0;
-  QGuiApplication application(zero, static_cast<char **>(nullptr));
-  MyWindow window;
-  window.setSource(QUrl("qrc:ui.qml"));
-
-  window.show();
+  QCoreApplication application(zero, static_cast<char **>(nullptr));
+  QObject eventSender;
 
   auto invokeManager = std::shared_ptr<InvokeManager>(new InvokeManager());
   ScxmlImporter importer(QUrl::fromLocalFile("../config/simple_state.xml"),
-                         &window, invokeManager);
+                         &eventSender, invokeManager);
   auto stateMachine = importer.getStateMachine();
-  std::cout << "running:" << stateMachine->isRunning() << std::endl;
 
   stateMachine->start();
 
-  std::cout << "running:" << stateMachine->isRunning() << std::endl;
-  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  application.processEvents(QEventLoop::ExcludeSocketNotifiers);
+  EXPECT_TRUE(stateMachine->isRunning());
 
-  std::cout << "error:" << stateMachine->errorString().toStdString()
-            << std::endl;
-  std::cout << "running:" << stateMachine->isRunning() << std::endl;
+  auto configuration = stateMachine->configuration();
+  EXPECT_EQ(1, configuration.size());
+  EXPECT_EQ("idle", (*configuration.begin())->property("name").toString());
 
-  stateMachine->postEvent(
-      new QKeyEvent(QEvent::KeyPress, Qt::Key_A, Qt::NoModifier));
+  application.sendEvent(&eventSender,
+      new QKeyEvent(QEvent::KeyPress, Qt::Key_B, Qt::NoModifier));
+  application.processEvents();
 
-  application.exec();
+  configuration = stateMachine->configuration();
+  EXPECT_EQ(1, configuration.size());
+  EXPECT_EQ("exit", (*configuration.begin())->property("name").toString());
+
+  EXPECT_FALSE(stateMachine->isRunning());
 }
 
