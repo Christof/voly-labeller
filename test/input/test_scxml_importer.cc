7cea399cc1aa54119333a3bcc5c46e1f500524f2
Christof Sirk <christof.sirk@gmail.com>
2015-03-30 20:20:46 +0200

    Extract test fixture.

--- test/input/test_scxml_importer.cc
+++ test/input/test_scxml_importer.cc
@@ -8,29 +8,47 @@
 #include <chrono>
 #include "../../src/input/scxml_importer.h"
 
-TEST(Test_ScxmlImporter, SwitchFromInitialToFinalStateWithKeyPress)
+class Test_ScxmlImporter : public ::testing::Test
 {
+ protected:
+  virtual void SetUp()
+  {
   int zero = 0;
-  QCoreApplication application(zero, static_cast<char **>(nullptr));
-  QObject eventSender;
+    application = new QCoreApplication(zero, static_cast<char **>(nullptr));
+    eventSender = new QObject();
 
-  auto invokeManager = std::shared_ptr<InvokeManager>(new InvokeManager());
-  ScxmlImporter importer(QUrl::fromLocalFile("../config/simple_state.xml"),
-                         &eventSender, invokeManager);
-  auto stateMachine = importer.getStateMachine();
+    auto invokeManager = std::shared_ptr<InvokeManager>(new InvokeManager());
+    ScxmlImporter importer(QUrl::fromLocalFile("../config/simple_state.xml"),
+                           eventSender, invokeManager);
+    stateMachine = importer.getStateMachine();
 
-  stateMachine->start();
+    stateMachine->start();
 
-  application.processEvents(QEventLoop::ExcludeSocketNotifiers);
+    application->processEvents(QEventLoop::ExcludeSocketNotifiers);
+  }
+
+  virtual void TearDown()
+  {
+    delete eventSender;
+    delete application;
+  }
+
+  QCoreApplication *application;
+  QObject *eventSender;
+  std::shared_ptr<QStateMachine> stateMachine;
+};
+
+TEST_F(Test_ScxmlImporter, SwitchFromInitialToFinalStateWithKeyPress)
+{
   EXPECT_TRUE(stateMachine->isRunning());
 
   auto configuration = stateMachine->configuration();
   EXPECT_EQ(1, configuration.size());
   EXPECT_EQ("idle", (*configuration.begin())->property("name").toString());
 
-  application.sendEvent(&eventSender,
-      new QKeyEvent(QEvent::KeyPress, Qt::Key_B, Qt::NoModifier));
-  application.processEvents();
+  application->sendEvent(
+      eventSender, new QKeyEvent(QEvent::KeyPress, Qt::Key_B, Qt::NoModifier));
+  application->processEvents();
 
   configuration = stateMachine->configuration();
   EXPECT_EQ(1, configuration.size());
