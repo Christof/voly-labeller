b150648f1f26036c750d8ea662746163c20c67b2
Christof Sirk <christof.sirk@gmail.com>
2015-04-04 10:57:35 +0200

    Disable logging in tests.

--- test/main.cc
+++ test/main.cc
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 
+#include <QLoggingCategory>
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
+
+  QLoggingCategory::setFilterRules("*=false");
 
   return RUN_ALL_TESTS();
 }
