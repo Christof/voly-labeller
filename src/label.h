01c400d9aa928846556c9863030a24415e7e62a3
Christof Sirk <christof.sirk@gmail.com>
2015-04-06 21:38:37 +0200

    Persistence of labels.

--- src/label.h
+++ src/label.h
@@ -3,6 +3,7 @@
 #define SRC_LABEL_H_
 
 #include <Eigen/Core>
+#include <boost/serialization/nvp.hpp>
 
 /**
  * \brief Holds basic data of a label: id, text, anchor position
@@ -11,6 +12,15 @@
 struct Label
 {
  public:
+  /**
+   * \brief Default constructor is only implemented for persistence
+   *
+   * \warn This should not be used.
+   */
+  Label() : Label(-1, "", Eigen::Vector3f())
+  {
+  }
+
   Label(int id, std::string text, Eigen::Vector3f anchorPosition)
     : id(id), text(text), anchorPosition(anchorPosition)
   {
@@ -20,5 +30,19 @@ struct Label
   std::string text;
   Eigen::Vector3f anchorPosition;
 };
+
+namespace boost
+{
+namespace serialization
+{
+template <class Archive>
+void serialize(Archive &ar, Label &label, const unsigned int version)
+{
+  ar &BOOST_SERIALIZATION_NVP(label.id);
+  ar &BOOST_SERIALIZATION_NVP(label.text);
+  ar &BOOST_SERIALIZATION_NVP(label.anchorPosition);
+}
+}  // namespace serialization
+}  // namespace boost
 
 #endif  // SRC_LABEL_H_
--- test/utils/test_qobject_persistence.cc
+++ test/utils/test_qobject_persistence.cc
@@ -1,6 +1,7 @@
 #include "../test.h"
 #include <Eigen/Core>
 #include "../../src/utils/persister.h"
+#include "../../src/label.h"
 
 TEST(Test_QObjectPersistence, SaveAndLoadASimpleString)
 {
@@ -18,4 +19,19 @@ TEST(Test_QObjectPersistence, SaveAndLoadAVector)
   Persister::save(vector, "test.xml");
   auto loaded = Persister::load<Eigen::Vector3f>("test.xml");
   EXPECT_NEAR(vector.x(), loaded.x(), 0.00001f);
+  EXPECT_NEAR(vector.y(), loaded.y(), 0.00001f);
+  EXPECT_NEAR(vector.z(), loaded.z(), 0.00001f);
+}
+
+TEST(Test_QObjectPersistence, SaveAndLoadALabel)
+{
+  Label label(2, "my label 2", Eigen::Vector3f(1, 2, 3));
+
+  Persister::save(label, "test.xml");
+  auto loaded = Persister::load<Label>("test.xml");
+  EXPECT_EQ(label.id, loaded.id);
+  EXPECT_EQ(label.text, loaded.text);
+  EXPECT_NEAR(label.anchorPosition.x(), loaded.anchorPosition.x(), 0.00001f);
+  EXPECT_NEAR(label.anchorPosition.y(), loaded.anchorPosition.y(), 0.00001f);
+  EXPECT_NEAR(label.anchorPosition.z(), loaded.anchorPosition.z(), 0.00001f);
 }
