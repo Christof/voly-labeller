ef17410eb9a3432cdb15ee6a1703a5cb3b79907e
Christof Sirk <christof.sirk@gmail.com>
2015-04-13 20:42:51 +0200

    Add import functionality.

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,6 +1,9 @@
 #include "./nodes.h"
 #include <QDebug>
 #include "./utils/persister.h"
+#include "./importer.h"
+#include "./gl.h"
+#include "./mesh_node.h"
 
 Nodes::Nodes()
 {
@@ -19,6 +22,24 @@ void Nodes::addSceneNodesFrom(std::string filename)
 
   for (auto &m : loadedNodes)
     nodes.push_back(m);
+}
+
+void Nodes::importFrom(std::string filename)
+{
+  Importer importer(Gl::instance);
+
+  auto meshes = importer.importAll(filename);
+
+  for (size_t i = 0; i < meshes.size(); ++i)
+  {
+    nodes.push_back(
+        std::make_shared<MeshNode>(filename, i, meshes[i], Eigen::Matrix4f()));
+  }
+}
+
+void Nodes::importFrom(QUrl url)
+{
+  importFrom(url.path().toStdString());
 }
 
 void Nodes::render(RenderData renderData)
