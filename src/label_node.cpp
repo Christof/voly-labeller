a13872033ac280350321612e4c6e771a147c26af
Christof Sirk <christof.sirk@gmail.com>
2015-04-14 18:58:20 +0200

    Render a mesh for the label anchor.

--- src/label_node.cpp
+++ src/label_node.cpp
@@ -1,6 +1,8 @@
 #include "./label_node.h"
 #include <QPainter>
 #include <QPoint>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 #include "./importer.h"
 #include "./mesh.h"
 
@@ -8,7 +10,7 @@ LabelNode::LabelNode(Label label) : label(label)
 {
   Importer importer;
 
-  anchorMesh = importer.import("../assets/sphere.dae", 0);
+  anchorMesh = importer.import("../assets/anchor.dae", 0);
 }
 
 LabelNode::~LabelNode()
@@ -17,24 +19,15 @@ LabelNode::~LabelNode()
 
 void LabelNode::render(Gl *gl, RenderData renderData)
 {
-  anchorMesh->render(gl, renderData.projectionMatrix, renderData.viewMatrix);
+  Eigen::Affine3f transform(Eigen::Translation3f(label.anchorPosition) *
+                            Eigen::Scaling(0.005f));
+  renderData.modelMatrix = transform.matrix();
+  anchorMesh->render(gl, renderData);
   QPainter painter;
   painter.begin(gl->paintDevice);
   painter.setPen(Qt::blue);
   painter.setFont(QFont("Arial", 16));
   painter.drawText(QRectF(10, 30, 300, 20), Qt::AlignLeft, "Label 1");
 
-  Eigen::Vector4f anchorPosition4D;
-  anchorPosition4D.head<3>() = label.anchorPosition;
-  anchorPosition4D.w() = 1.0f;
-  auto anchorPosition2D =
-      renderData.projectionMatrix * renderData.viewMatrix * anchorPosition4D;
-  QPoint anchorScreen(
-      (anchorPosition2D.x() / anchorPosition2D.w() * 0.5f + 0.5f) *
-          gl->size.width(),
-      (anchorPosition2D.y() / anchorPosition2D.w() * -0.5f + 0.5f) *
-          gl->size.height());
-  painter.drawArc(QRect(anchorScreen, QSize(4, 4)), 0, 5760);
-  painter.drawPoint(anchorScreen);
   painter.end();
 }
