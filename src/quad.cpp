c8a0849cefa2147db07625b3ff59c590983beda7
Christof Sirk <christof.sirk@gmail.com>
2015-04-14 21:58:37 +0200

    Add Quad class which will be used for labels.

---
+++ src/quad.cpp
@@ -0,0 +1,71 @@
+#include "./quad.h"
+#include "./gl.h"
+
+Quad::Quad()
+{
+}
+
+Quad::~Quad()
+{
+}
+
+void Quad::initialize(Gl *gl)
+{
+  shaderProgram = std::unique_ptr<ShaderProgram>(
+      new ShaderProgram(gl, ":shader/phong.vert", ":shader/phong.frag"));
+
+  vertexArrayObject.create();
+  vertexArrayObject.bind();
+
+  shaderProgram->bind();
+
+  float positions[12]{ -1.0f, 1.0f,  0.0f, 1.0f,  1.0f,  0.0f,
+                       1.0f,  -1.0f, 0.0f, -1.0f, -1.0f, 0.0f };
+  createBuffer(QOpenGLBuffer::Type::VertexBuffer, positions, "position", 3, 12);
+  float texcoords[8]{ 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f };
+  createBuffer(QOpenGLBuffer::Type::VertexBuffer, texcoords, "texcoord", 2, 12);
+  unsigned int indices[indexCount]{ 0, 1, 2, 0, 2, 3 };
+  createBuffer(QOpenGLBuffer::Type::IndexBuffer, indices, "index", 1,
+               indexCount);
+}
+
+void Quad::render(Gl *gl, const RenderData &renderData)
+{
+  if (!shaderProgram.get())
+    initialize(gl);
+
+  shaderProgram->bind();
+
+  Eigen::Matrix4f modelViewProjection = renderData.projectionMatrix *
+                                        renderData.viewMatrix *
+                                        renderData.modelMatrix;
+  shaderProgram->setUniform("modelViewProjectionMatrix", modelViewProjection);
+  shaderProgram->setUniform("modelMatrix", renderData.modelMatrix);
+
+  vertexArrayObject.bind();
+
+  glAssert(gl->glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0));
+
+  vertexArrayObject.release();
+  shaderProgram->release();
+}
+
+template <class ElementType>
+void Quad::createBuffer(QOpenGLBuffer::Type bufferType, ElementType *data,
+                        std::string usage, int perVertexElements,
+                        int numberOfVertices)
+{
+  QOpenGLBuffer buffer(bufferType);
+  buffer.create();
+  buffer.setUsagePattern(QOpenGLBuffer::StaticDraw);
+  buffer.bind();
+  buffer.allocate(data,
+                  numberOfVertices * perVertexElements * sizeof(ElementType));
+  glCheckError();
+
+  if (bufferType != QOpenGLBuffer::Type::IndexBuffer)
+    shaderProgram->enableAndSetAttributes(usage, perVertexElements);
+
+  buffers.push_back(buffer);
+}
+
---
+++ src/quad.h
@@ -0,0 +1,38 @@
+#ifndef SRC_QUAD_H_
+
+#define SRC_QUAD_H_
+
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+#include <vector>
+#include <memory>
+#include "./shader_program.h"
+#include "./render_data.h"
+
+/**
+ * \brief
+ *
+ *
+ */
+class Quad
+{
+ public:
+  Quad();
+  virtual ~Quad();
+
+  void render(Gl *gl, const RenderData &renderData);
+
+  void initialize(Gl *gl);
+
+ private:
+  QOpenGLVertexArrayObject vertexArrayObject;
+  std::vector<QOpenGLBuffer> buffers;
+  std::unique_ptr<ShaderProgram> shaderProgram;
+  static const int indexCount = 6;
+  template <class ElementType>
+  void createBuffer(QOpenGLBuffer::Type bufferType, ElementType *data,
+                    std::string usage, int perVertexElements,
+                    int numberOfVertices);
+};
+
+#endif  // SRC_QUAD_H_
