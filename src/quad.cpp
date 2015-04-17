4a0ef2bde861410859f8b79b26e978a10184967a
Christof Sirk <christof.sirk@gmail.com>
2015-04-17 15:17:17 +0200

    Fix billboard rendering.

--- src/quad.cpp
+++ src/quad.cpp
@@ -40,44 +40,13 @@ void Quad::render(Gl *gl, const RenderData &renderData,
 
   shaderProgram->bind();
 
-  Eigen::Vector3f cameraPosition = renderData.cameraPosition;
-  Eigen::Vector3f cameraUp =
-      renderData.viewMatrix.block<3, 3>(0, 0) * Eigen::Vector3f(0, 1, 0);
   Eigen::Vector3f labelPosition = renderData.modelMatrix.col(3).head(3);
-  Eigen::Vector3f look = (cameraPosition - labelPosition).normalized();
-  Eigen::Vector3f right = cameraUp.cross(look);
-  Eigen::Vector3f up = look.cross(right);
-
-  Eigen::Matrix4f modelMatrix = Eigen::Matrix4f::Identity();
-  modelMatrix.col(0).head(3) = right;
-  modelMatrix.col(1).head(3) = up;
-  modelMatrix.col(2).head(3) = look;
-  modelMatrix.col(3).head(3) = labelPosition;
-  /*
-  Eigen::Matrix3f inverseRot;
-  Eigen::Matrix3f rot = renderData.viewMatrix.block<3, 3>(0, 0);
-  bool invertible;
-  rot.computeInverseWithCheck(inverseRot, invertible);
-  Eigen::Matrix4f inverseView = Eigen::Matrix4f::Identity();
-  inverseView.block<3, 3>(0, 0) = inverseRot;
-
-  std::cout << "view: " << renderData.viewMatrix.format(Eigen::IOFormat()) <<
-  std::endl;
-  std::cout << "inverse: " << inverseView.format(Eigen::IOFormat()) <<
-  std::endl;
-
-  Eigen::Matrix4f modelView =
-      renderData.viewMatrix * inverseView * renderData.modelMatrix;
-
-  modelView(0, 1) = modelView(0, 2) = 0.0f;
-  modelView(1, 0) = modelView(1, 2) = 0.0f;
-  modelView(2, 0) = modelView(2, 1) = 0.0f;
-  */
-
   Eigen::Matrix4f modelViewProjection =
-      renderData.projectionMatrix * renderData.viewMatrix * modelMatrix;
+      renderData.projectionMatrix * renderData.viewMatrix;
   shaderProgram->setUniform("modelViewProjectionMatrix", modelViewProjection);
-  shaderProgram->setUniform("modelMatrix", renderData.modelMatrix);
+  shaderProgram->setUniform("viewMatrix", renderData.viewMatrix);
+  shaderProgram->setUniform("labelPosition", labelPosition);
+  shaderProgram->setUniform("size", Eigen::Vector2f(2.0f * 0.07f, 0.5f * 0.07f));
   shaderProgram->setUniform("textureSampler", 0);
 
   texture->bind(gl, GL_TEXTURE0);
