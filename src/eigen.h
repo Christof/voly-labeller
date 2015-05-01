7ac552c91207fc8971471da48993d4ff4d62f59b
Christof Sirk <christof.sirk@gmail.com>
2015-05-01 16:45:45 +0200

    Add file with eigen enhancements.

---
+++ src/eigen.h
@@ -0,0 +1,14 @@
+#ifndef SRC_EIGEN_H_
+
+#define SRC_EIGEN_H_
+
+#include <Eigen/Core>
+
+inline Eigen::Vector4f mul(const Eigen::Matrix4f &matrix,
+                    const Eigen::Vector3f &vector)
+{
+  Eigen::Vector4f operand(vector.x(), vector.y(), vector.z(), 1.0f);
+  return matrix * operand;
+}
+
+#endif  // SRC_EIGEN_H_
