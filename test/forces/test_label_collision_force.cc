58366e5d2ab4ea784011243a25744af8ee35dfe6
Christof Sirk <christof.sirk@gmail.com>
2015-05-04 19:48:57 +0200

    Add first test for LabelCollisionForce.

---
+++ test/forces/test_label_collision_force.cc
@@ -0,0 +1,27 @@
+#include "../test.h"
+#include "../../src/forces/label_collision_force.h"
+#include "../../src/forces/label_state.h"
+
+namespace Forces
+{
+
+TEST(Test_LabelCollisionForce, NoForceIfLabelsDontCollide)
+{
+  LabelCollisionForce force;
+
+  Eigen::Vector2f size(0.1f, 0.1f);
+  LabelState label(1, "Tested label", Eigen::Vector3f(0, 0, 0), size);
+  label.labelPosition2D = Eigen::Vector2f(0, 0);
+
+  LabelState other(2, "Other label", Eigen::Vector3f(1, 1, 0), size);
+  label.labelPosition2D = Eigen::Vector2f(1, 1);
+
+  LabellerFrameData frameData(1.0f, Eigen::Matrix4f::Identity(),
+                              Eigen::Matrix4f::Identity());
+  auto labels = std::vector<LabelState>{ label, other };
+  auto result = force.calculateForce(label, labels, frameData);
+
+  EXPECT_Vector2f_NEAR(Eigen::Vector2f(0, 0), result, 0.0001f);
+}
+
+}  // namespace Forces
