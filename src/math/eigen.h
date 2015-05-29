93a1b894958bf6a05186f1493fc60e6b6c535a78
Christof Sirk <christof.sirk@gmail.com>
2015-05-29 17:11:31 +0200

    Add function to Vector3f to eigen.h.

--- src/math/eigen.h
+++ src/math/eigen.h
@@ -12,6 +12,11 @@ inline Eigen::Vector4f mul(const Eigen::Matrix4f &matrix,
   return matrix * operand;
 }
 
+inline Eigen::Vector3f toVector3f(const Eigen::Vector4f vector)
+{
+  return Eigen::Vector3f(vector.x(), vector.y(), vector.z());
+}
+
 inline Eigen::Vector2f toEigen(const QPoint &pos)
 {
   return Eigen::Vector2f(pos.x(), pos.y());
