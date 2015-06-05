abe3180843745e21a56769b8be3a0445d2b236f6
Christof Sirk <christof.sirk@gmail.com>
2015-06-05 21:23:29 +0200

    Add test for updating of labels.

--- test/test_labels.cc
+++ test/test_labels.cc
@@ -47,8 +47,37 @@ TEST(Test_Labels, Remove)
                                              });
   labels.remove(label);
 
-  ASSERT_EQ(0, labels.count());
-  ASSERT_EQ(1, changedLabelId);
-  ASSERT_EQ(Labels::Action::Delete, receivedAction);
+  EXPECT_EQ(0, labels.count());
+  EXPECT_EQ(1, changedLabelId);
+  EXPECT_EQ(Labels::Action::Delete, receivedAction);
+
+  removeSubscription();
+}
+
+TEST(Test_Labels, UpdateTriggersANotification)
+{
+  Labels labels;
+
+  Label label(1, "Label text", Eigen::Vector3f(1, 2, 3));
+  labels.add(label);
+
+  label.text = "changed text";
+
+  int changedLabelId = -1;
+  Labels::Action receivedAction;
+  auto removeSubscription = labels.subscribe([&changedLabelId, &receivedAction](
+      Labels::Action action, const Label &label)
+                                             {
+                                               receivedAction = action;
+                                               changedLabelId = label.id;
+                                             });
+
+  labels.update(label);
+
+  EXPECT_EQ(1, changedLabelId);
+  EXPECT_EQ(Labels::Action::Update, receivedAction);
+  EXPECT_EQ("changed text", labels.getById(1).text);
+
+  removeSubscription();
 }
 
