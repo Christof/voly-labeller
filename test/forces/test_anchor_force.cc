0e4565c27691c0afffcac3e5387576bb5e242a5f
Christof Sirk <christof.sirk@gmail.com>
2015-06-15 18:05:04 +0200

    Add test for forces.

--- test/forces/test_anchor_force.cc
+++ test/forces/test_anchor_force.cc
@@ -23,4 +23,20 @@ TEST(Test_AnchorForce, PullsLabelToAnchor)
   Eigen::Vector2f expected = Eigen::Vector2f(-1, -1) * 0.085786f;
   EXPECT_Vector2f_NEAR(expected, result, 0.0001f);
 }
+
+TEST(Test_Force, ReturnsZeroVectorIfForceIsDisabled)
+{
+  AnchorForce force;
+  force.isEnabled = false;
+
+  Eigen::Vector2f size(0.1f, 0.1f);
+  LabelState label(1, "Tested label", Eigen::Vector3f(1, 1, 0), size);
+  LabellerFrameData frameData(1.0f, Eigen::Matrix4f::Identity(),
+                              Eigen::Matrix4f::Identity());
+  auto labels = std::vector<LabelState>{ label };
+  auto result = force.calculateForce(label, labels, frameData);
+
+  EXPECT_Vector2f_NEAR(Eigen::Vector2f::Zero(), result, 1E-5f);
+}
+
 }
