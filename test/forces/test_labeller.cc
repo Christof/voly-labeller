10f6cbdc21b14b395ddecdeb578c89c30b4a0b2d
Christof Sirk <christof.sirk@gmail.com>
2015-06-15 17:27:12 +0200

    Extract test fixture for labeller tests.

--- test/forces/test_labeller.cc
+++ test/forces/test_labeller.cc
@@ -2,20 +2,36 @@
 #include "../../src/forces/labeller.h"
 #include "../../src/labelling/labels.h"
 
-TEST(Test_Labeller, AddedLabelIsUpdated)
+class TestLabeller : public ::testing::Test
 {
-  auto labels = std::make_shared<Labels>();
-  Forces::Labeller labeller(labels);
+ protected:
+  virtual void SetUp()
+  {
+    labels = std::make_shared<Labels>();
+    labeller = std::make_shared<Forces::Labeller>(labels);
+  } 
+  // virtual void TearDown() {}
 
+  std::shared_ptr<Labels> labels;
+  std::shared_ptr<Forces::Labeller> labeller;
+};
+
+TEST_F(TestLabeller, AddedLabelIsUpdated)
+{
   Label label(1, "Label text", Eigen::Vector3f(1, 2, 3));
   labels->add(label);
 
   double frameTime = 1.0;
   Forces::LabellerFrameData frameData(frameTime, Eigen::Matrix4f::Identity(),
                                       Eigen::Matrix4f::Identity());
-  auto newPositions = labeller.update(frameData);
+
+  auto oldPosition = labeller->getLabels()[0].labelPosition;
+  auto newPositions = labeller->update(frameData);
 
   EXPECT_EQ(1, newPositions.size());
+  EXPECT_NE(oldPosition.x(), newPositions[0].x());
+
   EXPECT_Vector3f_NEAR(newPositions[label.id],
-                       labeller.getLabels()[0].labelPosition, 1E-5);
+                       labeller->getLabels()[0].labelPosition, 1E-5);
 }
+
