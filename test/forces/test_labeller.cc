1024c0843fb4218b56eaa18272a7b50a16d9bb0a
Christof Sirk <christof.sirk@gmail.com>
2015-06-15 17:52:43 +0200

    Add some more labeller tests.

--- test/forces/test_labeller.cc
+++ test/forces/test_labeller.cc
@@ -9,29 +9,74 @@ class TestLabeller : public ::testing::Test
   {
     labels = std::make_shared<Labels>();
     labeller = std::make_shared<Forces::Labeller>(labels);
-  } 
-  // virtual void TearDown() {}
+
+    labels->add(label);
+  }
 
   std::shared_ptr<Labels> labels;
   std::shared_ptr<Forces::Labeller> labeller;
+
+  Label label = { 1, "Label text", Eigen::Vector3f(1, 2, 3) };
+
+  Forces::LabellerFrameData getDefaultFrameData()
+  {
+    double frameTime = 1.0;
+    return Forces::LabellerFrameData(frameTime, Eigen::Matrix4f::Identity(),
+                                     Eigen::Matrix4f::Identity());
+  }
 };
 
 TEST_F(TestLabeller, AddedLabelIsUpdated)
 {
-  Label label(1, "Label text", Eigen::Vector3f(1, 2, 3));
-  labels->add(label);
-
-  double frameTime = 1.0;
-  Forces::LabellerFrameData frameData(frameTime, Eigen::Matrix4f::Identity(),
-                                      Eigen::Matrix4f::Identity());
-
   auto oldPosition = labeller->getLabels()[0].labelPosition;
-  auto newPositions = labeller->update(frameData);
+
+  auto newPositions = labeller->update(getDefaultFrameData());
 
   EXPECT_EQ(1, newPositions.size());
-  EXPECT_NE(oldPosition.x(), newPositions[0].x());
+  EXPECT_NE(oldPosition.x(), newPositions[label.id].x());
+}
+
+TEST_F(TestLabeller, FromUpdateReturnedPositionsMatchGetPositions)
+{
+  auto newPositions = labeller->update(getDefaultFrameData());
 
   EXPECT_Vector3f_NEAR(newPositions[label.id],
                        labeller->getLabels()[0].labelPosition, 1E-5);
+}
+
+TEST_F(TestLabeller, LabelHasSameDepthValueAsAnchor)
+{
+  auto newPositions = labeller->update(getDefaultFrameData());
+
+  EXPECT_NEAR(label.anchorPosition.z(), newPositions[label.id].z(), 1E-5);
+}
+
+TEST_F(TestLabeller,
+       LabelUpdateSetsTheGivenAnchorPositionAndReinitializesTheLabelPosition)
+{
+  auto newPositions = labeller->update(getDefaultFrameData());
+
+  label.anchorPosition = Eigen::Vector3f(-1, -2, -3);
+  labels->update(label);
+
+  EXPECT_NE(newPositions[label.id].x(),
+            labeller->getLabels()[0].labelPosition.x());
+  EXPECT_NE(newPositions[label.id].y(),
+            labeller->getLabels()[0].labelPosition.y());
+  EXPECT_NE(newPositions[label.id].z(),
+            labeller->getLabels()[0].labelPosition.z());
+}
+
+TEST_F(TestLabeller, LabelUpdateSetsTheCurrentLabelPositionIfAnchorsUnchanged)
+{
+  auto newPositions = labeller->update(getDefaultFrameData());
+
+  label.text = "Some changed text";
+  labels->update(label);
+
+  auto newLabelState = labeller->getLabels()[0];
+  EXPECT_EQ(label.text, newLabelState.text);
+  EXPECT_Vector3f_NEAR(newPositions[label.id], newLabelState.labelPosition,
+                       1E-5);
 }
 
