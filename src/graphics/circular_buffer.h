a5b4ba2f63ccc698b8643f4a2b2ed53b55d60c38
christof <christof.sirk@gmail.com>
2015-07-18 11:30:25 +0200

    Add CircularBuffer.

---
+++ src/graphics/circular_buffer.h
@@ -0,0 +1,74 @@
+#ifndef SRC_GRAPHICS_CIRCULAR_BUFFER_H_
+
+#define SRC_GRAPHICS_CIRCULAR_BUFFER_H_
+
+#include "./gl.h"
+#include "./mapped_buffer.h"
+
+namespace Graphics
+{
+
+template <typename T> class CircularBuffer : public MappedBuffer<T>
+{
+ public:
+  CircularBuffer(GLenum target, bool runUpdatesOnCPU = true)
+    : MappedBuffer<T>(target, runUpdatesOnCPU)
+  {
+  };
+
+  bool initialize(Gl *gl, GLuint count, GLbitfield createFlags,
+                  GLbitfield mapFlags)
+  {
+    this->gl = gl;
+    head = 0;
+    return MappedBuffer<T>::initialize(gl, count, createFlags, mapFlags);
+  }
+
+  T *reserve(GLsizeiptr count)
+  {
+    assert(count <= this->size());
+
+    GLsizeiptr lockStart = head;
+
+    if (lockStart + count > this->size())
+    {
+      // wrap
+      lockStart = 0;
+    }
+
+    this->waitForLockedRange(lockStart, count);
+    return &this->contents()[lockStart];
+  }
+
+  void onUsageComplete(GLsizeiptr count)
+  {
+    this->lockRange(head, count);
+    head = (head + count) % this->size();
+  }
+
+  void bindBufferRange(GLuint index, GLsizeiptr count)
+  {
+    assert(count <= this->mCount);
+
+    glAssert(gl->glBindBufferRange(this->getTarget(), index, this->id,
+                                   head * sizeof(T), count * sizeof(T)));
+  }
+
+  GLsizeiptr getHead() const
+  {
+    return head;
+  }
+
+  void *headOffset() const
+  {
+    return (void *)(head * sizeof(T));
+  }
+
+ private:
+  Gl *gl;
+  GLsizeiptr head;
+};
+
+}  // namespace Graphics
+
+#endif  // SRC_GRAPHICS_CIRCULAR_BUFFER_H_
