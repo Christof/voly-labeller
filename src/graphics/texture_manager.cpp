09145df528be5ece106836e9eb6676d98fa6957f
christof <christof.sirk@gmail.com>
2015-07-20 21:22:06 +0200

    Add TextureManager.

---
+++ src/graphics/texture_manager.cpp
@@ -0,0 +1,156 @@
+#include "./texture_manager.h"
+#include <vector>
+#include <cassert>
+#include <QImage>
+#include "./texture_container.h"
+#include "./texture2d.h"
+#include <iostream>
+
+namespace Graphics
+{
+
+int TextureManager::addTexture(std::string path)
+{
+  int id = textures.size();
+
+  textures.push_back(newTexture2d(path));
+
+  return id;
+}
+
+Texture2d *TextureManager::newTexture2d(GLsizei levels, GLenum internalformat,
+                                        GLsizei width, GLsizei height)
+{
+  assert(mInited);
+  Texture2d *retTex = allocateTexture2d(levels, internalformat, width, height);
+  retTex->commit();
+
+  return retTex;
+}
+
+Texture2d *TextureManager::newTexture2d(std::string path)
+{
+  try
+  {
+    auto image = new QImage(path.c_str());
+    GLenum internalformat = GL_RGBA;
+    GLenum format = GL_BGRA;
+    GLenum type = GL_UNSIGNED_BYTE;
+
+    Texture2d *retTex =
+        allocateTexture2d(1, internalformat, image->width(), image->height());
+    retTex->commit();
+
+    retTex->texSubImage2D(0, 0, 0, image->width(), image->height(), format,
+                          type, image->bits());
+
+    return retTex;
+  }
+  catch (std::exception &error)
+  {
+    qCritical() << "Error loading texture '" << path.c_str()
+                << "': " << error.what();
+    throw;
+  }
+}
+
+bool TextureManager::initialize(Gl *gl, bool sparse, GLsizei maxNumTextures)
+{
+  this->gl = gl;
+  // gl_assert(mInited == false);
+
+  mMaxTextureArrayLevels = maxNumTextures;
+  mSparse = sparse;
+
+  if (sparse)
+  {
+    // if (!HasExtension(ARB_sparse_texture)) {
+    //    return false;
+    //}
+  }
+
+  if (maxNumTextures <= 0)
+  {
+    if (sparse)
+    {
+      glGetIntegerv(GL_MAX_SPARSE_ARRAY_TEXTURE_LAYERS_ARB,
+                    &mMaxTextureArrayLevels);
+    }
+    else
+    {
+      glGetIntegerv(GL_MAX_ARRAY_TEXTURE_LAYERS, &mMaxTextureArrayLevels);
+    }
+  }
+
+  // mInited = true;
+
+  return true;
+}
+
+void TextureManager::shutdown()
+{
+  for (auto containIt = mTexArrays2D.begin(); containIt != mTexArrays2D.end();
+       ++containIt)
+  {
+    for (auto ptrIt = containIt->second.begin();
+         ptrIt != containIt->second.end(); ++ptrIt)
+    {
+      {
+        delete *ptrIt;
+        *ptrIt = nullptr;
+      }
+    }
+  }
+
+  mTexArrays2D.clear();
+
+  mInited = false;
+}
+
+Texture2d *TextureManager::allocateTexture2d(GLsizei levels,
+                                             GLenum internalformat,
+                                             GLsizei width, GLsizei height)
+{
+  TextureContainer *memArray = nullptr;
+
+  // compute power of two texture size
+  int twidth = 1;
+  int theight = 1;
+  while (twidth < width)
+    twidth <<= 1;
+  while (theight < height)
+    theight <<= 1;
+
+  std::cout << " width/height: " << width << "/" << height << " -> " << twidth
+            << " " << theight << std::endl;
+
+  auto texType = std::make_tuple(levels, internalformat, twidth, theight);
+  auto arrayIt = mTexArrays2D.find(texType);
+  if (arrayIt == mTexArrays2D.end())
+  {
+    mTexArrays2D[texType] = std::vector<TextureContainer *>();
+    arrayIt = mTexArrays2D.find(texType);
+    assert(arrayIt != mTexArrays2D.end());
+  }
+
+  for (auto it = arrayIt->second.begin(); it != arrayIt->second.end(); ++it)
+  {
+    if ((*it)->hasRoom())
+    {
+      memArray = (*it);
+      break;
+    }
+  }
+
+  if (memArray == nullptr)
+  {
+    memArray = new TextureContainer(gl, mSparse, levels, internalformat, twidth,
+                                    theight, mMaxTextureArrayLevels);
+    arrayIt->second.push_back(memArray);
+  }
+
+  assert(memArray);
+  return new Texture2d(memArray, memArray->virtualAlloc());
+}
+
+}  // namespace Graphics
---
+++ src/graphics/texture_manager.h
@@ -0,0 +1,55 @@
+#ifndef SRC_GRAPHICS_TEXTURE_MANAGER_H_
+
+#define SRC_GRAPHICS_TEXTURE_MANAGER_H_
+
+#include <string>
+#include <vector>
+#include <memory>
+#include "./texture.h"
+#include "./gl.h"
+
+namespace Graphics
+{
+
+class TextureContainer;
+class Texture2d;
+
+/**
+ * \brief
+ *
+ *
+ */
+class TextureManager
+{
+ public:
+  TextureManager() = default;
+
+  int addTexture(std::string path);
+
+  Texture2d *newTexture2d(GLsizei levels, GLenum internalformat, GLsizei width,
+                          GLsizei height);
+  Texture2d *newTexture2d(std::string path);
+
+  void free(Texture2d *_tex);
+
+  // maxNumTextures <= 0 will cause allocation of maximum number of layers
+  bool initialize(Gl *gl, bool sparse = true, GLsizei maxNumTextures = -1);
+  void shutdown();
+
+ private:
+  Gl *gl;
+  std::vector<Texture2d *> textures;
+  std::map<std::tuple<GLsizei, GLenum, GLsizei, GLsizei>,
+           std::vector<TextureContainer *>> mTexArrays2D;
+  bool mInited;
+  GLsizei mMaxTextureArrayLevels;
+
+  bool mSparse;
+
+  Texture2d *allocateTexture2d(GLsizei levels, GLenum internalformat,
+                               GLsizei width, GLsizei height);
+};
+
+}  // namespace Graphics
+
+#endif  // SRC_GRAPHICS_TEXTURE_MANAGER_H_
