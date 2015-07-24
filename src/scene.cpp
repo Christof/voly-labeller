c7ff705da8584c5e4746362d2b4bba02ce9ff8e1
christof <christof.sirk@gmail.com>
2015-07-24 15:48:42 +0200

    Increase buffer size to fit other meshes.

--- src/scene.cpp
+++ src/scene.cpp
@@ -57,7 +57,7 @@ void Scene::initialize()
       std::make_shared<Graphics::HABuffer>(Eigen::Vector2i(width, height));
   haBuffer->initialize(gl);
 
-  bufferManager->initialize(gl, 128, 1000);
+  bufferManager->initialize(gl, 128, 10000000);
   std::vector<float> positions = { 1.0f, 1.0f,  0.0f, -1.0f, 1.0f,  0.0f,
                                    1.0f, -1.0f, 0.0f, -1.0f, -1.0f, 0.0f };
   std::vector<float> normals = { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
