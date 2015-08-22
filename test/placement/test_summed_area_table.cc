13d4db3aa3189579d087972846ba23069379f4d6
christof <christof.sirk@gmail.com>
2015-08-22 15:28:19 +0200

    Add thrust test using a custom kernel.

--- test/placement/test_summed_area_table.cc
+++ test/placement/test_summed_area_table.cc
@@ -1,8 +1,14 @@
 #include "../test.h"
 
 int sumUsingThrustReduce();
+int sumUsingCuda();
 
 TEST(Test_SummedAreaTable, SumUsingThrustReduce)
 {
   EXPECT_EQ(6.0f, sumUsingThrustReduce());
+}
+
+TEST(Test_SummedAreaTable, SumUsingCuda)
+{
+  EXPECT_EQ(6.0f, sumUsingCuda());
 }
