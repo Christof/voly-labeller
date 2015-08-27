45335f6266e3318ddd4caa7b716845893edd17bc
christof <christof.sirk@gmail.com>
2015-08-27 22:16:20 +0200

    Introduce algSAT host function and use it for testing.

--- test/placement/test_summed_area_table.cc
+++ test/placement/test_summed_area_table.cc
@@ -1,8 +1,10 @@
 #include "../test.h"
+#include <thrust/host_vector.h>
 
 int sumUsingThrustReduce();
 int sumUsingCuda();
 int sumUsingCudaInLib();
+thrust::host_vector<float> algSAT(float *h_inout, int w, int h);
 
 TEST(Test_SummedAreaTable, SumUsingThrustReduce)
 {
@@ -17,4 +19,17 @@ TEST(Test_SummedAreaTable, SumUsingCuda)
 TEST(Test_SummedAreaTable, SumUsingCudaInLib)
 {
   EXPECT_EQ(6.0f, sumUsingCudaInLib());
+}
+
+TEST(Test_SummedAreaTable, SAT)
+{
+  std::vector<float> input = { 1, 2, 3, 4 };
+  thrust::host_vector<float> result = algSAT(input.data(), 2, 2);
+
+  ASSERT_LE(4, result.size());
+
+  EXPECT_EQ(1, result[0]);
+  EXPECT_EQ(3, result[1]);
+  EXPECT_EQ(4, result[2]);
+  EXPECT_EQ(10, result[3]);
 }
