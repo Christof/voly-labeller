619db7b7361d7946cc7b6c2412fe42f2d5a6f95f
christof <christof.sirk@gmail.com>
2015-09-27 22:25:25 +0200

    Add CudaTextureMapper::getResource and remove unused code.

--- src/placement/cuda_texture_mapper.cpp
+++ src/placement/cuda_texture_mapper.cpp
@@ -1,18 +1,11 @@
 #include "./cuda_texture_mapper.h"
 
-cudaError registerImage(cudaGraphicsResource *resource, unsigned int id)
-{
-  return cudaGraphicsGLRegisterImage(&resource, id, GL_RENDERBUFFER,
-                                     cudaGraphicsRegisterFlagsNone);
-}
-
 CudaTextureMapper::CudaTextureMapper(unsigned int textureId)
   : textureId(textureId)
 {
-  // cudaError error = registerImage(resource, textureId);
   qInfo() << "map texture" << textureId;
-  cudaError error = cudaGraphicsGLRegisterImage(&resource, textureId, GL_TEXTURE_2D,
-                                                cudaGraphicsRegisterFlagsNone);
+  cudaError error = cudaGraphicsGLRegisterImage(
+      &resource, textureId, GL_TEXTURE_2D, cudaGraphicsRegisterFlagsNone);
 
   qWarning() << "register image" << cudaGetErrorString(error);
 }
@@ -20,4 +13,9 @@ CudaTextureMapper::CudaTextureMapper(unsigned int textureId)
 CudaTextureMapper::~CudaTextureMapper()
 {
   cudaGraphicsUnregisterResource(resource);
+}
+
+cudaGraphicsResource **CudaTextureMapper::getResource()
+{
+  return &resource;
 }
