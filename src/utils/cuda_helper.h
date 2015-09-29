d5105abdf717676f4801fb5e31c7cb0f62f11eab
christof <christof.sirk@gmail.com>
2015-09-29 22:26:54 +0200

    Move static function into macro to circumvent unused function waringn-erros.

--- src/utils/cuda_helper.h
+++ src/utils/cuda_helper.h
@@ -7,16 +7,15 @@
 #include <iostream>
 #include <stdexcept>
 
-static void HandleError(cudaError_t error, const char *file, int line)
-{
-  if (error != cudaSuccess)
-  {
-    printf("%s in %s at line %d\n", cudaGetErrorString(error), file, line);
-    throw std::runtime_error(cudaGetErrorString(error));
+#define HANDLE_ERROR(error)                                                    \
+  {                                                                            \
+    if (error != cudaSuccess)                                                  \
+    {                                                                          \
+      printf("%s in %s at line %d\n", cudaGetErrorString(error), __FILE__,     \
+             __LINE__);                                                        \
+      throw std::runtime_error(cudaGetErrorString(error));                     \
+    }                                                                          \
   }
-}
-
-#define HANDLE_ERROR(error) (HandleError(error, __FILE__, __LINE__))
 
 #define HANDLE_NULL(a)                                                         \
   {                                                                            \
