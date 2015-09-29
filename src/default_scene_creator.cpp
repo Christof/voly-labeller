e60a49b33724135b7314ddc499efdebc03fd5936
christof <christof.sirk@gmail.com>
2015-09-29 18:05:29 +0200

    Scene with multiple volumes.

--- src/default_scene_creator.cpp
+++ src/default_scene_creator.cpp
@@ -23,11 +23,19 @@ void DefaultSceneCreator::create()
   std::vector<std::shared_ptr<Node>> sceneNodes;
   addMeshNodesTo(sceneNodes);
   addLabelNodesTo(sceneNodes);
+  /*
   sceneNodes.push_back(
       std::make_shared<VolumeNode>("assets/datasets/neurochirurgie_test.mhd",
                                    "assets/transferfunctions/scapula4.gra"));
   sceneNodes.push_back(std::make_shared<VolumeNode>(
       "assets/datasets/MR-head.nrrd", "assets/transferfunctions/scapula1.gra"));
+      */
+  sceneNodes.push_back(
+      std::make_shared<VolumeNode>("assets/datasets/GRCH_Abdomen.mhd",
+                                   "assets/transferfunctions/MANIX_skin.gra"));
+  sceneNodes.push_back(
+      std::make_shared<VolumeNode>("assets/datasets/GRCH_Schaedel_fein_H31.mhd",
+                                   "assets/transferfunctions/scapula1.gra"));
   Persister::save(sceneNodes, "config/scene.xml");
 
   // nodes->addSceneNodesFrom("config/scene.xml");
