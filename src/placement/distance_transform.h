c0b669e5cfe258343bc7c49c1eca0a3bfc1c19fd
christof <christof.sirk@gmail.com>
2015-10-05 16:39:59 +0200

    Extract distance_transform.h and change signature to use cudaArray.
    This is so that it can be tested without creating an OpenGl texture.

---
+++ src/placement/distance_transform.h
@@ -0,0 +1,9 @@
+#include <thrust/device_vector.h>
+
+void cudaJFAApolloniusThrust(cudaArray_t imageArray, int imageSize, int numLabels,
+                            thrust::device_vector<float4> &seedbuffer,
+                            thrust::device_vector<float> &distance_vector,
+                            thrust::device_vector<int> &compute_vector,
+                            thrust::device_vector<int> &compute_temp_vector,
+                            thrust::device_vector<int> &compute_seed_ids,
+                            thrust::device_vector<int> &compute_seed_indices);
