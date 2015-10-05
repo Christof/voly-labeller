d66da6383c46ed2bce670931c6281c7d5e2fd726
christof <christof.sirk@gmail.com>
2015-10-05 19:21:56 +0200

    Add algSAT declaration to header and use it in test.

--- src/placement/summed_area_table.h
+++ src/placement/summed_area_table.h
@@ -2,9 +2,12 @@
 
 #define SRC_PLACEMENT_SUMMED_AREA_TABLE_H_
 
+#include <thrust/host_vector.h>
 #include <cuda_runtime.h>
 #include <memory>
 #include "./cuda_texture_mapper.h"
+
+thrust::host_vector<float> algSAT(float *h_inout, int w, int h);
 
 void toGray(std::shared_ptr<CudaTextureMapper> tex, int image_size);
 
