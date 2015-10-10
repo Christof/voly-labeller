a10854ff656b56695ad7fc099e7ed0368017e3ae
christof <christof.sirk@gmail.com>
2015-10-10 20:03:43 +0200

    Add test for distance transform.

--- src/placement/distance_transform.cu
+++ src/placement/distance_transform.cu
@@ -3,7 +3,6 @@
 #include <thrust/device_ptr.h>
 #include "../utils/cuda_helper.h"
 
-
 surface<void, cudaSurfaceType2D> surfaceWrite;
 texture<float, 2, cudaReadModeElementType> depthTexture;
 
@@ -56,45 +55,49 @@ __global__ void jfa_seed_kernel(int imageSize, int num_labels,
   surf2Dwrite<float4>(outval, surfaceWrite, x * sizeof(float4), y);
 }
 
-__global__ void apollonius_cuda(int *data,
-                                float *occupancy,
-                                unsigned int step, int w, int h)
+__global__ void apollonius_cuda(int *data, float *occupancy, unsigned int step,
+                                int w, int h)
 {
-  int x = blockIdx.x*blockDim.x + threadIdx.x;
-  int y = blockIdx.y*blockDim.y + threadIdx.y;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
   if (x >= w || y >= h)
     return;
 
-  int index = y*w + x;
+  int index = y * w + x;
 
   int cur_nearest = data[index];
-  int cur_y = cur_nearest/w;
-  int cur_x = cur_nearest - cur_y*w;
-  float curr_w = (cur_nearest< w*h) ? occupancy[cur_nearest] : 0.0f;
+  int cur_y = cur_nearest / w;
+  int cur_x = cur_nearest - cur_y * w;
+  float curr_w = (cur_nearest < w * h) ? occupancy[cur_nearest] : 0.0f;
 
-  float cur_dist = sqrtf(float((x-cur_x) * (x-cur_x) + (y-cur_y) * (y-cur_y))) - curr_w;
+  float cur_dist =
+      sqrtf(float((x - cur_x) * (x - cur_x) + (y - cur_y) * (y - cur_y))) -
+      curr_w;
 
 #pragma unroll
-  for (int i=-1; i<=1; i++)
+  for (int i = -1; i <= 1; i++)
   {
-    int u = x+i*step;
+    int u = x + i * step;
     if (u < 0 || u >= w)
       continue;
 #pragma unroll
-    for (int j=-1; j<=1; j+=2-i*i)
+    for (int j = -1; j <= 1; j += 2 - i * i)
     {
-      int v = y+j*step;
+      int v = y + j * step;
       if (v < 0 || v >= h)
         continue;
 
-      int newindex = v*w+u;
+      int newindex = v * w + u;
       int new_nearest = data[newindex];
-      int new_y = new_nearest/w;
-      int new_x = new_nearest - new_y*w;
-      float new_w = (new_nearest < w*h) ? occupancy[new_nearest] : 0.0f;
-      float new_dist = sqrtf(float((x-new_x) * (x-new_x) + (y-new_y) * (y-new_y))) - new_w;
+      int new_y = new_nearest / w;
+      int new_x = new_nearest - new_y * w;
+      float new_w = (new_nearest < w * h) ? occupancy[new_nearest] : 0.0f;
+      float new_dist =
+          sqrtf(float((x - new_x) * (x - new_x) + (y - new_y) * (y - new_y))) -
+          new_w;
 
-      if (new_dist < cur_dist || cur_nearest >= w*h) {
+      if (new_dist < cur_dist || cur_nearest >= w * h)
+      {
         cur_dist = new_dist;
         cur_nearest = new_nearest;
       }
@@ -104,31 +107,34 @@ __global__ void apollonius_cuda(int *data,
   data[index] = cur_nearest;
 }
 
-__global__ void  /*__launch_bounds__(16)*/ jfa_thrust_gather(int imageSize, int num_labels,
-                                                             int* thrustptr, int* seedidptr, int* seedidxptr)
+__global__ void /*__launch_bounds__(16)*/ jfa_thrust_gather(int imageSize,
+                                                            int num_labels,
+                                                            int *thrustptr,
+                                                            int *seedidptr,
+                                                            int *seedidxptr)
 {
-  int x = blockIdx.x*blockDim.x + threadIdx.x;
-  int y = blockIdx.y*blockDim.y + threadIdx.y;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
   if (x >= imageSize || y >= imageSize)
     return;
-  int index = y*imageSize + x;
+  int index = y * imageSize + x;
   float4 color;
   int labelID = 100;
   int labelIndex = thrustptr[index];
-  //int ly = labelID/imageSize;
-  //int lx = labelID - ly*imageSize;
+  // int ly = labelID/imageSize;
+  // int lx = labelID - ly*imageSize;
 
-  //bool foundcolor = false;
-  for (int i=0;i<num_labels;i++)
+  // bool foundcolor = false;
+  for (int i = 0; i < num_labels; i++)
   {
-    //int sly = seedptr[i]/imageSize;
-    //int slx = seedptr[i] - sly*imageSize;
-    //if (slx == lx && sly == ly)
+    // int sly = seedptr[i]/imageSize;
+    // int slx = seedptr[i] - sly*imageSize;
+    // if (slx == lx && sly == ly)
     //{
     if (labelIndex == seedidxptr[i])
     {
-      //foundcolor = true;
-      labelID=seedidptr[i];
+      // foundcolor = true;
+      labelID = seedidptr[i];
       break;
     }
   }
@@ -136,85 +142,93 @@ __global__ void  /*__launch_bounds__(16)*/ jfa_thrust_gather(int imageSize, int 
   switch (labelID)
   {
   case 0:
-    color = make_float4(0.0, 0.0, 0.0,1.0);
+    color = make_float4(0.0, 0.0, 0.0, 1.0);
     break;
   case 1:
-    color = make_float4(1.0, 0.0, 0.0,1.0);
+    color = make_float4(1.0, 0.0, 0.0, 1.0);
     break;
   case 2:
-    color = make_float4(0.0, 1.0, 0.0,1.0);
+    color = make_float4(0.0, 1.0, 0.0, 1.0);
     break;
   case 3:
-    color = make_float4(0.0, 0.0, 1.0,1.0);
+    color = make_float4(0.0, 0.0, 1.0, 1.0);
     break;
   case 4:
-    color = make_float4(1.0, 1.0, 0.0,1.0);
+    color = make_float4(1.0, 1.0, 0.0, 1.0);
     break;
   case 5:
-    color = make_float4(0.0, 1.0, 1.0,1.0);
+    color = make_float4(0.0, 1.0, 1.0, 1.0);
     break;
   case 6:
-    color = make_float4(1.0, 0.0, 1.0,1.0);
+    color = make_float4(1.0, 0.0, 1.0, 1.0);
     break;
   case 7:
-    color = make_float4(1.0, 1.0, 1.0,1.0);
+    color = make_float4(1.0, 1.0, 1.0, 1.0);
     break;
   default:
-    color = make_float4(0.5, 0.5, 0.5,1.0);
+    color = make_float4(0.5, 0.5, 0.5, 1.0);
   }
-  surf2Dwrite<float4>(color, surfaceWrite, x*sizeof(float4), y);
+  surf2Dwrite<float4>(color, surfaceWrite, x * sizeof(float4), y);
 }
 
-__global__ void jfa_dtf_init_kernel(int image_size, float xscale, float yscale, int* thrustptr)
+__global__ void jfa_dtf_init_kernel(int image_size, float xscale, float yscale,
+                                    int *thrustptr)
 {
-  int x = blockIdx.x*blockDim.x + threadIdx.x;
-  int y = blockIdx.y*blockDim.y + threadIdx.y;
-  int index = y*image_size + x;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  if (x >= image_size || y >= image_size)
+    return;
 
-  float texval = tex2D(depthTexture, x*xscale,y*yscale);
-  int outindex = (image_size*2)*(image_size*2)-1;
+  int index = y * image_size + x;
 
-  thrustptr[index] = (texval>=0.99f) ? index : outindex;
+  float texval = tex2D(depthTexture, x * xscale, y * yscale);
+  int outindex = (image_size * 2) * (image_size * 2) - 1;
+
+  thrustptr[index] = (texval >= 0.99f) ? index : outindex;
 }
 
-__global__ void jfa_cuda(int* data, unsigned int step, int w, int h)
+__global__ void jfa_cuda(int *data, unsigned int step, int w, int h)
 {
-  int x = blockIdx.x*blockDim.x + threadIdx.x;
-  int y = blockIdx.y*blockDim.y + threadIdx.y;
-  int index = y*w + x;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  if (x >= w || y >= h)
+    return;
+
+  int index = y * w + x;
 
   int cur_nearest = data[index];
-  int cur_y = cur_nearest/w;
-  int cur_x = cur_nearest - cur_y*w;
-  int cur_dist = (x-cur_x) * (x-cur_x) + (y-cur_y) * (y-cur_y);
+  int cur_y = cur_nearest / w;
+  int cur_x = cur_nearest - cur_y * w;
+  int cur_dist = (x - cur_x) * (x - cur_x) + (y - cur_y) * (y - cur_y);
 
-
-  //if (x>253 && x<257 && y==255)
+// if (x>253 && x<257 && y==255)
 //  if ((x==255 && y==255) || (x==10 && y==10))
 //  {
-//    printf("%d %d: step: %d cur: %d %d %d, %d %d\n", x, y, step, cur_nearest, cur_x, cur_y, w, h);
+//    printf("%d %d: step: %d cur: %d %d %d, %d %d\n", x, y, step, cur_nearest,
+//    cur_x, cur_y, w, h);
 //  }
 
 #pragma unroll
-  for (int i=-1; i<=1; i++)
+  for (int i = -1; i <= 1; i++)
   {
-    int u = x+i*step;
+    int u = x + i * step;
     if (u < 0 || u >= w)
       continue;
 #pragma unroll
-    for (int j=-1; j<=1; j+=2-i*i)
+    for (int j = -1; j <= 1; j += 2 - i * i)
     {
-      int v = y+j*step;
+      int v = y + j * step;
       if (v < 0 || v >= h)
         continue;
 
-      int newindex = v*w+u;
+      int newindex = v * w + u;
       int new_nearest = data[newindex];
-      int new_y = new_nearest/w;
-      int new_x = new_nearest - new_y*w;
-      int new_dist = (x-new_x) * (x-new_x) + (y-new_y) * (y-new_y);
+      int new_y = new_nearest / w;
+      int new_x = new_nearest - new_y * w;
+      int new_dist = (x - new_x) * (x - new_x) + (y - new_y) * (y - new_y);
 
-      if (new_dist < cur_dist) {
+      if (new_dist < cur_dist)
+      {
         cur_dist = new_dist;
         cur_nearest = new_nearest;
       }
@@ -224,26 +238,33 @@ __global__ void jfa_cuda(int* data, unsigned int step, int w, int h)
   data[index] = cur_nearest;
 }
 
-__global__ void  /*__launch_bounds__(16)*/ jfa_dtf_compute_distance_kernel(int image_size, int* index_ptr, float* value_ptr)
+__global__ void jfa_dtf_compute_distance_kernel(int image_size, int *index_ptr,
+                                                float *value_ptr)
 {
-  int x = blockIdx.x*blockDim.x + threadIdx.x;
-  int y = blockIdx.y*blockDim.y + threadIdx.y;
-  int index = y*image_size + x;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  if (x >= image_size || y >= image_size)
+    return;
+
+  int index = y * image_size + x;
   int voronoival = index_ptr[index];
 
-  int ty = voronoival/image_size;
-  int tx = voronoival - ty*image_size;
+  int ty = voronoival / image_size;
+  int tx = voronoival - ty * image_size;
 
-  float sqdist = ((tx-x)*(tx-x) + (ty-y)*(ty-y));
-  //float dist = sqdist;
+  float sqdist = ((tx - x) * (tx - x) + (ty - y) * (ty - y));
+  // float dist = sqdist;
   float distf = sqrtf(sqdist);
 
   value_ptr[index] = distf;
 
   // write to texture for debugging
-  float4 color = make_float4(16.0f*distf/image_size, 16.0f*distf/image_size, 16.0f*distf/image_size, 1.0f);
-  //float4 color = make_float4(512.0f*distf/image_size, 256.0f*distf/image_size, 16.0f*distf/image_size, 1.0f);
-  surf2Dwrite<float4>(color, surfaceWrite, x*sizeof(float4), y);
+  float4 color =
+      make_float4(16.0f * distf / image_size, 16.0f * distf / image_size,
+                  16.0f * distf / image_size, 1.0f);
+  // float4 color = make_float4(512.0f*distf/image_size,
+  // 256.0f*distf/image_size, 16.0f*distf/image_size, 1.0f);
+  surf2Dwrite<float4>(color, surfaceWrite, x * sizeof(float4), y);
 }
 
 void cudaJFADistanceTransformThrust(
@@ -279,18 +300,20 @@ void cudaJFADistanceTransformThrust(
     thrust::device_vector<int> &compute_temp_vector,
     thrust::device_vector<float> &result_vector)
 {
-  if (compute_vector.size() != static_cast<unsigned long>(image_size*image_size))
+  if (compute_vector.size() !=
+      static_cast<unsigned long>(image_size * image_size))
   {
-    compute_vector.resize(image_size*image_size, image_size*image_size);
-    compute_temp_vector.resize(image_size*image_size, image_size*image_size);
-    result_vector.resize(image_size*image_size, image_size*image_size);
+    compute_vector.resize(image_size * image_size, image_size * image_size);
+    compute_temp_vector.resize(image_size * image_size,
+                               image_size * image_size);
+    result_vector.resize(image_size * image_size, image_size * image_size);
   }
 
   int *compute_index_ptr = thrust::raw_pointer_cast(compute_vector.data());
   float *result_value_ptr = thrust::raw_pointer_cast(result_vector.data());
 
   dim3 dimBlock(32, 1, 1);
-  dim3 dimGrid(divUp(image_size,dimBlock.x), divUp(image_size,dimBlock.y), 1);
+  dim3 dimGrid(divUp(image_size, dimBlock.x), divUp(image_size, dimBlock.y), 1);
   {
     // read depth buffer and initialize distance transform computation
     depthTexture.normalized = 0;
@@ -300,8 +323,9 @@ void cudaJFADistanceTransformThrust(
 
     cudaBindTextureToArray(&depthTexture, inputImageArray, &inputImageDesc);
 
-    jfa_dtf_init_kernel<<<dimGrid,dimBlock>>>(image_size, float(screen_size_x)/float(image_size), float(screen_size_y)/float(image_size),
-                                              compute_index_ptr);
+    jfa_dtf_init_kernel << <dimGrid, dimBlock>>>
+        (image_size, float(screen_size_x) / float(image_size),
+         float(screen_size_y) / float(image_size), compute_index_ptr);
     cudaThreadSynchronize();
 
     cudaUnbindTexture(&depthTexture);
@@ -309,29 +333,37 @@ void cudaJFADistanceTransformThrust(
 
   // voronoi diagram computation in thrust
   {
-    //compute_temp_vector = compute_vector;
-    jfa_cuda<<<dimGrid,dimBlock>>>(thrust::raw_pointer_cast(compute_vector.data()),
-                                   1, image_size, image_size);
-    //compute_vector.swap(compute_temp_vector);
+    // compute_temp_vector = compute_vector;
+    jfa_cuda << <dimGrid, dimBlock>>>
+        (thrust::raw_pointer_cast(compute_vector.data()), 1, image_size,
+         image_size);
+    // compute_vector.swap(compute_temp_vector);
 
-    //cudaThreadSynchronize();
-    for(int k = (image_size / 2); k > 0; k /= 2)
+    // cudaThreadSynchronize();
+    for (int k = (image_size / 2); k > 0; k /= 2)
     {
-      jfa_cuda<<<dimGrid,dimBlock>>>(thrust::raw_pointer_cast(compute_vector.data()),
-                                     k, image_size, image_size);
-      //cudaThreadSynchronize();
-      //compute_vector.swap(compute_temp_vector);
+      jfa_cuda<<<dimGrid, dimBlock>>>(
+          thrust::raw_pointer_cast(compute_vector.data()), k, image_size,
+          image_size);
+      // cudaThreadSynchronize();
+      // compute_vector.swap(compute_temp_vector);
     }
 
     cudaThreadSynchronize();
   }
 
   {
-    cudaBindSurfaceToArray(surfaceWrite, outputImageArray);
+    cudaChannelFormatDesc outputChannelDesc =
+        cudaCreateChannelDesc(32, 32, 32, 32, cudaChannelFormatKindFloat);
+    HANDLE_ERROR(cudaBindSurfaceToArray(surfaceWrite, outputImageArray,
+                                        outputChannelDesc));
 
     // kernel which maps color to distance transform result
     compute_index_ptr = thrust::raw_pointer_cast(compute_vector.data());
-    jfa_dtf_compute_distance_kernel<<<dimGrid, dimBlock>>>(image_size, compute_index_ptr, result_value_ptr);
+    jfa_dtf_compute_distance_kernel<<<dimGrid, dimBlock>>>(
+        image_size, compute_index_ptr, result_value_ptr);
+
+    cudaThreadSynchronize();
   }
 }
 
@@ -370,15 +402,15 @@ void cudaJFAApolloniusThrust(cudaArray_t imageArray, int imageSize,
       cudaCreateChannelDesc(32, 32, 32, 32, cudaChannelFormatKindFloat);
   HANDLE_ERROR(cudaBindSurfaceToArray(surfaceWrite, imageArray, channelDesc));
 
-  jfa_seed_kernel<<<dimGrid, dimBlock>>>(imageSize, numLabels, seedBufferPtr,
-      raw_ptr, idptr, idxptr);
+  jfa_seed_kernel << <dimGrid, dimBlock>>>
+      (imageSize, numLabels, seedBufferPtr, raw_ptr, idptr, idxptr);
   HANDLE_ERROR(cudaThreadSynchronize());
 
   compute_temp_vector = compute_vector;
-  apollonius_cuda<<<dimGrid, dimBlock>>>(
-      thrust::raw_pointer_cast(compute_vector.data()),
-      thrust::raw_pointer_cast(distance_vector.data()), 1, imageSize,
-      imageSize);
+  apollonius_cuda << <dimGrid, dimBlock>>>
+      (thrust::raw_pointer_cast(compute_vector.data()),
+       thrust::raw_pointer_cast(distance_vector.data()), 1, imageSize,
+       imageSize);
   /*
 
   for (int k = (imageSize / 2); k > 0; k /= 2)
--- test/placement/test_distance_transform.cc
+++ test/placement/test_distance_transform.cc
@@ -1,9 +1,13 @@
 #include "../test.h"
 #include <Eigen/Core>
 
-void callDistanceTransform(std::vector<Eigen::Vector4f> &image);
+void callApollonoius(std::vector<Eigen::Vector4f> &image);
+std::vector<Eigen::Vector4f>
+callDistanceTransform(std::vector<float> depth,
+                      std::vector<float> &resultVector);
 
-TEST(Test_DistanceTransform, DistanceTransform)
+/*
+TEST(Test_DistanceTransform, Apollonoius)
 {
   std::vector<Eigen::Vector4f> image;
   image.push_back(Eigen::Vector4f::Zero());
@@ -26,8 +30,97 @@ TEST(Test_DistanceTransform, DistanceTransform)
   image.push_back(Eigen::Vector4f::Zero());
   image.push_back(Eigen::Vector4f::Zero());
 
-  callDistanceTransform(image);
+  callApollonoius(image);
 
-  EXPECT_Vector4f_NEAR(Eigen::Vector4f(1, 2, 3, 4), image[0], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[0], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[1], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[2], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[3], 1e-4f);
+
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[4], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[5], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[6], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[7], 1e-4f);
+
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[8], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[9], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[10], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[11], 1e-4f);
+
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[12], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[13], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[14], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[15], 1e-4f);
 }
+*/
 
+TEST(Test_DistanceTransform, DistanceTransform)
+{
+  std::vector<float> depthImage;
+
+  depthImage.push_back(1.0f);
+  depthImage.push_back(1.0f);
+  depthImage.push_back(1.0f);
+  depthImage.push_back(1.0f);
+
+  depthImage.push_back(1.0f);
+  depthImage.push_back(0.0f);
+  depthImage.push_back(0.0f);
+  depthImage.push_back(1.0f);
+
+  depthImage.push_back(1.0f);
+  depthImage.push_back(0.0f);
+  depthImage.push_back(0.0f);
+  depthImage.push_back(1.0f);
+
+  depthImage.push_back(1.0f);
+  depthImage.push_back(1.0f);
+  depthImage.push_back(1.0f);
+  depthImage.push_back(1.0f);
+
+  std::vector<float> resultVector;
+  auto image = callDistanceTransform(depthImage, resultVector);
+
+  EXPECT_EQ(16, resultVector.size());
+  EXPECT_NEAR(0.0f, resultVector[0], 1e-4f);
+  EXPECT_NEAR(0.0f, resultVector[1], 1e-4f);
+  EXPECT_NEAR(0.0f, resultVector[2], 1e-4f);
+  EXPECT_NEAR(0.0f, resultVector[3], 1e-4f);
+
+  EXPECT_NEAR(0.0f, resultVector[4], 1e-4f);
+  EXPECT_NEAR(1.0f, resultVector[5], 1e-4f);
+  EXPECT_NEAR(1.0f, resultVector[6], 1e-4f);
+  EXPECT_NEAR(1.0f, resultVector[7], 1e-4f);
+
+  EXPECT_NEAR(0.0f, resultVector[8], 1e-4f);
+  EXPECT_NEAR(1.0f, resultVector[9], 1e-4f);
+  EXPECT_NEAR(2.0f, resultVector[10], 1e-4f);
+  EXPECT_NEAR(2.0f, resultVector[11], 1e-4f);
+
+  EXPECT_NEAR(0.0f, resultVector[12], 1e-4f);
+  EXPECT_NEAR(1.0f, resultVector[13], 1e-4f);
+  EXPECT_NEAR(2.0f, resultVector[14], 1e-4f);
+  EXPECT_NEAR(3.0f, resultVector[15], 1e-4f);
+
+  /*
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[0], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[1], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[2], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[3], 1e-4f);
+
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[4], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[5], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[6], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[7], 1e-4f);
+
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[8], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[9], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[10], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[11], 1e-4f);
+
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[12], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[13], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[14], 1e-4f);
+  EXPECT_Vector4f_NEAR(Eigen::Vector4f(0.5f, 0.5f, 0.5f, 1), image[15], 1e-4f);
+  */
+}
