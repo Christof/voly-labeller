91bb83fe8bf2f4ca1cdc23aefdfa9f77142c5470
christof <christof.sirk@gmail.com>
2015-10-11 17:12:27 +0200

    Add width and height to CudaArrayProvider.

--- src/utils/cuda_array_provider.h
+++ src/utils/cuda_array_provider.h
@@ -11,11 +11,29 @@
 class CudaArrayProvider
 {
  public:
+  CudaArrayProvider(int width, int height) : width(width), height(height)
+  {
+  }
+
   virtual void map() = 0;
   virtual void unmap() = 0;
 
   virtual cudaChannelFormatDesc getChannelDesc() = 0;
   virtual cudaArray_t getArray() = 0;
+
+  int getWidth()
+  {
+    return width;
+  }
+
+  int getHeight()
+  {
+    return height;
+  }
+
+ protected:
+  int width;
+  int height;
 };
 
 #endif  // SRC_UTILS_CUDA_ARRAY_PROVIDER_H_
