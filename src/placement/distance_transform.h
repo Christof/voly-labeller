f9c8612114d2dc043429c1cab44f91e5fbd01172
christof <christof.sirk@gmail.com>
2015-10-13 16:25:14 +0200

    Move apollonius related code into own files.

--- src/placement/distance_transform.h
+++ src/placement/distance_transform.h
@@ -4,14 +4,6 @@
 
 #define MAX_LABELS 256
 
-void cudaJFAApolloniusThrust(cudaArray_t imageArray, int imageSize,
-                             int numLabels,
-                             thrust::device_vector<float4> &seedbuffer,
-                             thrust::device_vector<float> &distance_vector,
-                             thrust::device_vector<int> &compute_vector,
-                             thrust::device_vector<int> &compute_temp_vector,
-                             thrust::device_vector<int> &compute_seed_ids,
-                             thrust::device_vector<int> &compute_seed_indices);
 
 /**
  * \brief
@@ -35,7 +27,7 @@ class DistanceTransform
   int pixelCount;
   dim3 dimBlock;
   dim3 dimGrid;
-  cudaTextureObject_t inputTexture;
+  // cudaTextureObject_t inputTexture;
   void resize();
   void runInitializeKernel();
   void runStepsKernels();
