512dcff7071620ff3e5d54ebdf3a391bf8dc42b2
christof <christof.sirk@gmail.com>
2015-10-13 21:30:58 +0200

    Define example distances in test.

--- test/placement/test_apollonius.cu
+++ test/placement/test_apollonius.cu
@@ -5,7 +5,8 @@
 #include "../../src/utils/cuda_helper.h"
 #include "../cuda_array_mapper.h"
 
-void callApollonoius(std::vector<Eigen::Vector4f> &image)
+void callApollonoius(std::vector<Eigen::Vector4f> &image,
+                     std::vector<float> distances)
 {
   cudaChannelFormatDesc channelDesc =
       cudaCreateChannelDesc(32, 32, 32, 32, cudaChannelFormatKindFloat);
@@ -18,7 +19,7 @@ void callApollonoius(std::vector<Eigen::Vector4f> &image)
 
   thrust::device_vector<float4> seedBuffer(pixelCount, make_float4(0, 0, 0, 0));
   seedBuffer[0] = make_float4(1, 1, 1, 1);
-  thrust::device_vector<float> distanceVector(pixelCount, 0);
+  thrust::device_vector<float> distanceVector(distances);
   thrust::device_vector<int> computeVector;
   thrust::device_vector<int> computeVectorTemp;
   thrust::device_vector<int> computeSeedIds;
