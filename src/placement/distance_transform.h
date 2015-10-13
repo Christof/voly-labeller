82770af7f6d6807b2c4476eb4a662e92a0ba0be7
christof <christof.sirk@gmail.com>
2015-10-13 16:27:47 +0200

    Remove old single function distance transform versions.

--- src/placement/distance_transform.h
+++ src/placement/distance_transform.h
@@ -2,9 +2,6 @@
 #include <thrust/device_vector.h>
 #include "../utils/cuda_array_provider.h"
 
-#define MAX_LABELS 256
-
-
 /**
  * \brief
  *
@@ -34,16 +31,3 @@ class DistanceTransform
   void runFinishKernel();
 };
 
-void
-cudaJFADistanceTransformThrust(std::shared_ptr<CudaArrayProvider> inputImage,
-                               std::shared_ptr<CudaArrayProvider> outputImage,
-                               int image_size, int screen_size_x,
-                               int screen_size_y,
-                               thrust::device_vector<int> &compute_vector,
-                               thrust::device_vector<float> &result_vector);
-
-void cudaJFADistanceTransformThrust(
-    cudaArray_t inputImageArray, cudaChannelFormatDesc inputImageDesc,
-    cudaArray_t outputImageArray, int image_size, int screen_size_x,
-    int screen_size_y, thrust::device_vector<int> &compute_vector,
-    thrust::device_vector<float> &result_vector);
