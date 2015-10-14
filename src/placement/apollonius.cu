cadb43df2e9f96174b6187fce1f8153664489e1e
christof <christof.sirk@gmail.com>
2015-10-14 21:38:06 +0200

    Minor: remove unused code.

--- src/placement/apollonius.cu
+++ src/placement/apollonius.cu
@@ -25,7 +25,6 @@ __global__ void jfa_seed_kernel(int imageSize, int num_labels,
   for (int i = 0; i < num_labels; i++)
   {
     float4 seedval = seedbuffer[i];
-    // if (seedval.x > 0.0) outval = make_float4(0.0f, 1.0f, 0.0f, 1.0f);
     if (int(seedval.x) > 0 && x == int(seedval.y) && y == int(seedval.z) &&
         (x != 0 || y != 0))
     {
@@ -35,17 +34,6 @@ __global__ void jfa_seed_kernel(int imageSize, int num_labels,
 
       // index for thrust computation =
       outindex = x + y * imageSize;
-      // printf("hit: outval: %f %f %f %f %d %d %f %f %f nl: %d oi:
-      // %d\n",outval.x, outval.y, outval.z, outval.width, x, y, seedval.x,
-      // seedval.y, seedval.z,num_labels, outindex);
-      // printf("hit: outval: %f %f %f %f %d %d\n",outval.x, outval.y, outval.z,
-      // outval.width, x, y);
-      // break;
-    }
-    else
-    {
-      // printf("not hit: outval: %f %f %f %f %d %d %f %f %f \n",outval.x,
-      // outval.y, outval.z, outval.width, x, y, seedval.x, seedval.y, seedval.z);
     }
     idptr[i] = int(seedval.x);
     idxptr[i] = int(seedval.y) + int(seedval.z) * imageSize;
@@ -118,19 +106,11 @@ __global__ void jfa_thrust_gather(int imageSize, int num_labels, int *thrustptr,
   float4 color;
   int labelID = 100;
   int labelIndex = thrustptr[index];
-  // int ly = labelID/imageSize;
-  // int lx = labelID - ly*imageSize;
 
-  // bool foundcolor = false;
   for (int i = 0; i < num_labels; i++)
   {
-    // int sly = seedptr[i]/imageSize;
-    // int slx = seedptr[i] - sly*imageSize;
-    // if (slx == lx && sly == ly)
-    //{
     if (labelIndex == seedidxptr[i])
     {
-      // foundcolor = true;
       labelID = seedidptr[i];
       break;
     }
@@ -245,66 +225,6 @@ void Apollonius::runGatherKernel()
   int *raw_ptr = thrust::raw_pointer_cast(computeVector.data());
   int *idptr = thrust::raw_pointer_cast(seedIds.data());
   int *idxptr = thrust::raw_pointer_cast(seedIndices.data());
-  jfa_thrust_gather<<<dimGrid, dimBlock>>>(imageSize, numLabels, raw_ptr,
-      idptr, idxptr);
-  HANDLE_ERROR(cudaThreadSynchronize());
-}
-
-void cudaJFAApolloniusThrust(cudaArray_t imageArray, int imageSize,
-                             int numLabels,
-                             thrust::device_vector<float4> &seedbuffer,
-                             thrust::device_vector<float> &distance_vector,
-                             thrust::device_vector<int> &compute_vector,
-                             thrust::device_vector<int> &compute_temp_vector,
-                             thrust::device_vector<int> &compute_seed_ids,
-                             thrust::device_vector<int> &compute_seed_indices)
-{
-  int pixelCount = imageSize * imageSize;
-  if (compute_vector.size() != static_cast<unsigned long>(pixelCount))
-  {
-    compute_vector.resize(pixelCount, pixelCount);
-    compute_temp_vector.resize(pixelCount, pixelCount);
-  }
-
-  if (compute_seed_ids.size() != MAX_LABELS ||
-      compute_seed_indices.size() != MAX_LABELS)
-  {
-    compute_seed_ids.resize(MAX_LABELS, -1);
-    compute_seed_indices.resize(MAX_LABELS, -1);
-  }
-
-  int *raw_ptr = thrust::raw_pointer_cast(compute_vector.data());
-  int *idptr = thrust::raw_pointer_cast(compute_seed_ids.data());
-  int *idxptr = thrust::raw_pointer_cast(compute_seed_indices.data());
-  float4 *seedBufferPtr = thrust::raw_pointer_cast(seedbuffer.data());
-
-  dim3 dimBlock(32, 32, 1);
-  dim3 dimGrid(divUp(imageSize, dimBlock.x), divUp(imageSize, dimBlock.y), 1);
-
-  cudaChannelFormatDesc channelDesc =
-      cudaCreateChannelDesc(32, 32, 32, 32, cudaChannelFormatKindFloat);
-  HANDLE_ERROR(cudaBindSurfaceToArray(surfaceWrite, imageArray, channelDesc));
-
-  jfa_seed_kernel<<<dimGrid, dimBlock>>>
-      (imageSize, numLabels, seedBufferPtr, raw_ptr, idptr, idxptr);
-  HANDLE_ERROR(cudaThreadSynchronize());
-
-  compute_temp_vector = compute_vector;
-  apollonius_cuda<<<dimGrid, dimBlock>>>
-      (thrust::raw_pointer_cast(compute_vector.data()),
-       thrust::raw_pointer_cast(distance_vector.data()), 1, imageSize,
-       imageSize);
-
-  for (int k = (imageSize / 2); k > 0; k /= 2)
-  {
-    apollonius_cuda<<<dimGrid, dimBlock>>>(
-        thrust::raw_pointer_cast(compute_vector.data()),
-        thrust::raw_pointer_cast(distance_vector.data()), k, imageSize,
-        imageSize);
-  }
-  HANDLE_ERROR(cudaThreadSynchronize());
-
-  // colorize diagram
   jfa_thrust_gather<<<dimGrid, dimBlock>>>(imageSize, numLabels, raw_ptr,
       idptr, idxptr);
   HANDLE_ERROR(cudaThreadSynchronize());
