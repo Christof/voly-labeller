21b3218333a557d110ec2c56cbf74a5b659f8803
christof <christof.sirk@gmail.com>
2015-10-15 20:46:54 +0200

    Remove test_to_gray.cu by moving all to cc file.

--- test/placement/test_to_gray.cc
+++ test/placement/test_to_gray.cc
@@ -1,7 +1,22 @@
 #include "../test.h"
+#include "../../src/placement/to_gray.h"
+#include "../cuda_array_mapper.h"
 #include <thrust/host_vector.h>
 
-unsigned int toGrayUsingCuda(unsigned int value);
+unsigned int toGrayUsingCuda(unsigned int value)
+{
+  cudaChannelFormatDesc channelDesc =
+      cudaCreateChannelDesc(8, 8, 8, 8, cudaChannelFormatKindUnsigned);
+  std::vector<unsigned int> data = { value };
+  auto arrayProvider =
+      std::make_shared<CudaArrayMapper<unsigned int>>(1, 1, data, channelDesc);
+
+  ToGray(arrayProvider).runKernel();
+
+  auto resultVector = arrayProvider->copyDataFromGpu();
+
+  return resultVector[0];
+}
 
 TEST(Test_ToGray, toGray)
 {
