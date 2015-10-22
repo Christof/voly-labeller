c2e96aa54042d6d0eeef72ed5eb280c66b2d653e
christof <christof.sirk@gmail.com>
2015-10-22 15:56:04 +0200

    Add some more scaffolding for Labeller.

--- src/placement/labeller.cpp
+++ src/placement/labeller.cpp
@@ -7,4 +7,16 @@ Labeller::Labeller(std::shared_ptr<Labels> labels) : labels(labels)
 {
 }
 
+void Labeller::initialize(
+    std::shared_ptr<CudaTextureMapper> occupancySummedAreaTable)
+{
+  this->occupancySummedAreaTable = occupancySummedAreaTable;
+}
+
+std::map<int, Eigen::Vector3f>
+Labeller::update(const LabellerFrameData &frameData)
+{
+  return std::map<int, Eigen::Vector3f>();
+}
+
 }  // namespace Placement
--- src/placement/labeller.h
+++ src/placement/labeller.h
@@ -2,8 +2,12 @@
 
 #define SRC_PLACEMENT_LABELLER_H_
 
+#include <Eigen/Core>
 #include <memory>
 #include "../labelling/labels.h"
+#include "../labelling/labeller_frame_data.h"
+
+class CudaTextureMapper;
 
 namespace Placement
 {
@@ -18,8 +22,13 @@ class Labeller
  public:
   explicit Labeller(std::shared_ptr<Labels> labels);
 
+  void initialize(std::shared_ptr<CudaTextureMapper> occupancySummedAreaTable);
+
+  std::map<int, Eigen::Vector3f> update(const LabellerFrameData &frameData);
+
  private:
   std::shared_ptr<Labels> labels;
+  std::shared_ptr<CudaTextureMapper> occupancySummedAreaTable;
 };
 
 }  // namespace Placement
