a5ec9af82a3c5369448b1b4b059e5ed7394ff533
christof <christof.sirk@gmail.com>
2015-10-27 20:48:25 +0100

    Add test for larger summed area table 32 x 32 which seems to work.

--- test/placement/test_summed_area_table.cc
+++ test/placement/test_summed_area_table.cc
@@ -39,3 +39,36 @@ TEST(Test_SummedAreaTable, SAT)
   EXPECT_EQ(24, result[6]);
   EXPECT_EQ(36, result[7]);
 }
+
+TEST(Test_SummedAreaTable, SATLarger)
+{
+  std::vector<float> input(32 * 32, 0);
+  for (int i = 0; i < 4; ++i)
+  {
+    input[i] = i + 1;
+    input[i + 32] = i + 5;
+    input[i + 64] = i + 1;
+    input[i + 96] = i + 5;
+  }
+  cudaChannelFormatDesc channelDesc =
+      cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
+  auto inputImageProvider =
+      std::make_shared<CudaArrayMapper<float>>(32, 32, input, channelDesc);
+
+  SummedAreaTable table(inputImageProvider);
+  table.runKernel();
+  thrust::host_vector<float> result = table.getResults();
+
+  ASSERT_LE(32 * 32, result.size());
+
+  EXPECT_EQ(1, result[0]);
+  EXPECT_EQ(3, result[1]);
+  EXPECT_EQ(6, result[2]);
+  EXPECT_EQ(10, result[3]);
+  EXPECT_EQ(10, result[4]);
+  EXPECT_EQ(10, result[31]);
+  EXPECT_EQ(6, result[32]);
+  EXPECT_EQ(14, result[33]);
+  EXPECT_EQ(24, result[34]);
+  EXPECT_EQ(36, result[35]);
+}
