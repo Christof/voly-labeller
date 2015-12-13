da8cf0f0f6c0546dcb1301bad7057c169bf1fa69
christof <christof.sirk@gmail.com>
2015-12-13 10:53:39 +0100

    Fix QImageDrawer.

--- test/placement/test_constraint_updater.cc
+++ test/placement/test_constraint_updater.cc
@@ -21,12 +21,22 @@ class QImageDrawer : public Graphics::Drawer
     std::vector<QPointF> points;
     for (size_t i = 0; i < positions.size() / 2; ++i)
     {
-      points.push_back(QPointF(positions[i * 2], positions[i * 2 + 1]));
+      float x = positions[i * 2];
+      float y = positions[i * 2 + 1];
+      if (i * 2 + 3 < positions.size() && x == positions[i * 2 + 2] &&
+          y == positions[i * 2 + 3])
+      {
+        painter.drawConvexPolygon(points.data(), points.size());
+        points.clear();
+        i += 2;
+      }
+      points.push_back(QPointF(x, y));
     }
     painter.drawPolygon(points.data(), points.size(),
-                        Qt::FillRule::WindingFill);
+                        Qt::FillRule::OddEvenFill);
     painter.end();
   }
+
   void clear()
   {
     image->fill(Qt::GlobalColor::black);
@@ -37,8 +47,7 @@ TEST(Test_ConstraintUpdater, Draw)
 {
   int width = 512;
   int height = 256;
-  auto drawer =
-      std::make_shared<QImageDrawer>(width, height);
+  auto drawer = std::make_shared<QImageDrawer>(width, height);
   ConstraintUpdater constraintUpdater(drawer, width, height);
 
   constraintUpdater.clear();
@@ -46,7 +55,7 @@ TEST(Test_ConstraintUpdater, Draw)
   Eigen::Vector2i anchorPosition(100, 50);
   Eigen::Vector2i labelSize(62, 20);
   Eigen::Vector2i lastAnchorPosition(200, 70);
-  Eigen::Vector2i lastLabelPosition(200, 50);
+  Eigen::Vector2i lastLabelPosition(150, 120);
   Eigen::Vector2i lastLabelSize(60, 40);
   constraintUpdater.drawConstraintRegionFor(anchorPosition, labelSize,
                                             lastAnchorPosition,
