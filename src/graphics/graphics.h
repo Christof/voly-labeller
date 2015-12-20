8e836a41737ff059b8f042357860a04199d29f9c
christof <christof.sirk@gmail.com>
2015-12-20 14:16:54 +0100

    Add documentation for Graphics and Math namespaces.

---
+++ src/graphics/graphics.h
@@ -0,0 +1,15 @@
+#ifndef SRC_GRAPHICS_GRAPHICS_H_
+
+#define SRC_GRAPHICS_GRAPHICS_H_
+
+/**
+ * \brief Contains classses related to the whole rendering system
+ *
+ * This includes classes for buffers, textures, shaders, volumes and meshes.
+ */
+namespace Graphics
+{
+
+}  // namespace Graphics
+
+#endif  // SRC_GRAPHICS_GRAPHICS_H_
---
+++ src/math/math.h
@@ -0,0 +1,16 @@
+#ifndef SRC_MATH_MATH_H_
+
+#define SRC_MATH_MATH_H_
+
+/**
+ * \brief Contains classes related to mathematical calculations or constructs.
+ *
+ * This mainly includes bounding volumes for collision detection. Also some
+ * extensions to Eigen are provided.
+ */
+namespace Math
+{
+
+}  // namespace Math
+
+#endif  // SRC_MATH_MATH_H_
