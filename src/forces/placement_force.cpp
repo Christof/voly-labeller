c73ee9eae7909a351a57e27c06382f335b15fa64
christof <christof.sirk@gmail.com>
2016-01-07 17:32:58 +0100

    PlacementForce is zero if placement result is not set.

--- src/forces/placement_force.cpp
+++ src/forces/placement_force.cpp
@@ -12,6 +12,10 @@ PlacementForce::PlacementForce() : Force("Placement", 2.0f)
 Eigen::Vector2f PlacementForce::calculate(LabelState &label,
                                           std::vector<LabelState> &labels)
 {
+  if (isnan(label.placementPosition2D.x()) &&
+      isnan(label.placementPosition2D.y()))
+    return Eigen::Vector2f(0, 0);
+
   return (label.placementPosition2D - label.labelPosition2D).normalized();
 }
 
--- test/forces/test_placement_force.cc
+++ test/forces/test_placement_force.cc
@@ -24,4 +24,22 @@ TEST(Test_PlacementForce, PullsLabelToPlacementResult)
   EXPECT_Vector2f_NEAR(expected, result, 0.0001f);
 }
 
+TEST(Test_PlacementForce, DoesntExertForceIfNoPlacementResultAvailable)
+{
+  PlacementForce force;
+  force.weight = 1.0f;
+
+  Eigen::Vector2f size(0.1f, 0.1f);
+  LabelState label(1, "Tested label", Eigen::Vector3f(1, 1, 0), size);
+  label.labelPosition2D = Eigen::Vector2f(1, 1);
+  std::cout << label.placementPosition2D << std::endl;
+
+  LabellerFrameData frameData(1.0f, Eigen::Matrix4f::Identity(),
+                              Eigen::Matrix4f::Identity());
+  auto labels = std::vector<LabelState>{ label };
+  auto result = force.calculateForce(label, labels, frameData);
+
+  EXPECT_Vector2f_NEAR(Eigen::Vector2f(0, 0), result, 0.0001f);
+}
+
 }
