292422060530a5d9ffc30cdb5c1953305b100bc6
christof <christof.sirk@gmail.com>
2016-01-10 14:59:22 +0100

    Complete label equality test.

--- test/labelling/test_label.cc
+++ test/labelling/test_label.cc
@@ -5,8 +5,25 @@ TEST(Test_Label, EqualityIsDeterminedByIdTextAnchorPositionAndSize)
 {
   Label label1(0, "Label 0", Eigen::Vector3f(1, 2, 3));
   Label label2(0, "Label 0", Eigen::Vector3f(1, 2, 3));
+  Label label3(1, "Label 0", Eigen::Vector3f(1, 2, 3));
+  Label label4(0, "Label 1", Eigen::Vector3f(1, 2, 3));
+  Label label5(0, "Label 0", Eigen::Vector3f(0, 0, 0));
 
   EXPECT_TRUE(label1 == label2);
   EXPECT_FALSE(label1 != label2);
-}
 
+  EXPECT_FALSE(label1 == label3);
+  EXPECT_TRUE(label1 != label3);
+
+  EXPECT_FALSE(label2 == label3);
+  EXPECT_TRUE(label2 != label3);
+
+  EXPECT_FALSE(label1 == label4);
+  EXPECT_TRUE(label1 != label4);
+
+  EXPECT_FALSE(label2 == label4);
+  EXPECT_TRUE(label2 != label4);
+
+  EXPECT_FALSE(label1 == label5);
+  EXPECT_TRUE(label1 != label5);
+}
