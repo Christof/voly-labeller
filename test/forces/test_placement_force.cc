a3afbe64d96230c78c62b5e61e7f6c059644a1a1
christof <christof.sirk@gmail.com>
2016-03-11 15:16:49 +0100

    Scale placement force down if the label is already near the target position.

--- test/forces/test_placement_force.cc
+++ test/forces/test_placement_force.cc
@@ -24,6 +24,25 @@ TEST(Test_PlacementForce, PullsLabelToPlacementResult)
   EXPECT_Vector2f_NEAR(expected, result, 0.0001f);
 }
 
+TEST(Test_PlacementForce, ScalesForceDownIfLabelIsAlreadyNearThePlacementResult)
+{
+  PlacementForce force;
+  force.weight = 1.0f;
+
+  Eigen::Vector2f size(0.1f, 0.1f);
+  LabelState label(1, "Tested label", Eigen::Vector3f(1, 1, 0), size);
+  label.labelPosition2D = Eigen::Vector2f(0.01f, 0.01f);
+  label.placementPosition2D = Eigen::Vector2f(0, 0);
+
+  LabellerFrameData frameData(1.0f, Eigen::Matrix4f::Identity(),
+                              Eigen::Matrix4f::Identity());
+  auto labels = std::vector<LabelState>{ label };
+  auto result = force.calculateForce(label, labels, frameData);
+
+  Eigen::Vector2f expected = Eigen::Vector2f(-0.01f, -0.01f);
+  EXPECT_Vector2f_NEAR(expected, result, 0.0001f);
+}
+
 TEST(Test_PlacementForce, DoesntExertForceIfNoPlacementResultAvailable)
 {
   PlacementForce force;
