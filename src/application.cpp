ac21c558641301ffa6e7f426f7d2bb55ed587431
christof <christof.sirk@gmail.com>
2016-03-15 21:34:17 +0100

    Move instantiation of LabellingCoordinator into Application.

--- src/application.cpp
+++ src/application.cpp
@@ -14,6 +14,7 @@
 #include "./labeller_model.h"
 #include "./labels_model.h"
 #include "./labelling/labels.h"
+#include "./labelling_coordinator.h"
 #include "./picking_controller.h"
 #include "./forces_visualizer_node.h"
 #include "./default_scene_creator.h"
@@ -21,6 +22,8 @@
 #include "./texture_mapper_manager_controller.h"
 #include "./utils/memory.h"
 #include "./utils/path_helper.h"
+
+const int LAYER_COUNT = 4;
 
 Application::Application(int &argc, char **argv) : application(argc, argv)
 {
@@ -30,14 +33,16 @@ Application::Application(int &argc, char **argv) : application(argc, argv)
 
   labels = std::make_shared<Labels>();
   forcesLabeller = std::make_shared<Forces::Labeller>(labels);
+  auto labellingCoordinator = std::make_shared<LabellingCoordinator>(
+      LAYER_COUNT, forcesLabeller, labels, nodes);
 
   const int postProcessingTextureSize = 512;
   textureMapperManager =
       std::make_shared<TextureMapperManager>(postProcessingTextureSize);
   textureMapperManagerController =
       std::make_unique<TextureMapperManagerController>(textureMapperManager);
-  scene = std::make_shared<Scene>(invokeManager, nodes, labels, forcesLabeller,
-                                  textureMapperManager);
+  scene = std::make_shared<Scene>(LAYER_COUNT, invokeManager, nodes, labels,
+                                  labellingCoordinator, textureMapperManager);
 
   window = std::make_unique<Window>(scene);
   sceneController = std::make_unique<SceneController>(scene);
