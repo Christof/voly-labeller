c03396b39d59f2627053e510fc20297e4c5ac33f
christof <christof.sirk@gmail.com>
2016-03-20 12:22:25 +0100

    Start to implement Model for placement labeller.

---
+++ src/placement_labeller_model.h
@@ -0,0 +1,53 @@
+#ifndef SRC_PLACEMENT_LABELLER_MODEL_H_
+
+#define SRC_PLACEMENT_LABELLER_MODEL_H_
+
+#include <QAbstractTableModel>
+#include <memory>
+#include "./placement/cost_function_calculator.h"
+
+class LabellingCoordinator;
+
+/**
+ * \brief Model to change weights and settings of placement labeller
+ *
+ */
+class PlacementLabellerModel : public QAbstractTableModel
+{
+  Q_OBJECT
+ public:
+  explicit PlacementLabellerModel(
+      std::shared_ptr<LabellingCoordinator> coordinator);
+
+  enum WeightRoles
+  {
+    NameRole = Qt::UserRole + 1,
+    WeightRole,
+  };
+
+  QHash<int, QByteArray> roleNames() const Q_DECL_OVERRIDE;
+
+  int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+  int
+  columnCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+
+  QVariant data(const QModelIndex &index,
+                int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+
+  Qt::ItemFlags flags(const QModelIndex &index) const Q_DECL_OVERRIDE;
+
+  bool getIsVisible() const;
+
+ public slots:
+  void changeWeight(int row, QVariant newValue);
+
+ private:
+  std::shared_ptr<LabellingCoordinator> coordinator;
+
+  Placement::CostFunctionWeights weights;
+
+  QString getWeightNameForRowIndex(int rowIndex) const;
+  float getWeightValueForRowIndex(int rowIndex) const;
+};
+
+#endif  // SRC_PLACEMENT_LABELLER_MODEL_H_
