899da3cc2cbaa610cc5aa3f078ee3db304a6e2d6
christof <christof.sirk@gmail.com>
2016-03-21 19:27:05 +0100

    Start to enable a different weighting for connector shadow regions.
    Not yet working, because of problems with blending/depth for
    multiple layers and overlapping regions of weights below 1.0.

--- src/graphics/qimage_drawer.h
+++ src/graphics/qimage_drawer.h
@@ -23,7 +23,7 @@ class QImageDrawer : public Drawer
 
   QImageDrawer(int width, int height);
 
-  void drawElementVector(std::vector<float> positions);
+  void drawElementVector(std::vector<float> positions, float weight);
 
   void clear();
 };
