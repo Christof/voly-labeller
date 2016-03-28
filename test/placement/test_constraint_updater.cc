414ca54854cb32184897c2e1f01418ed35a245c4
christof <christof.sirk@gmail.com>
2016-03-28 11:45:54 +0200

    Extract Fixture for Test_ConstraintUpdater.

--- test/placement/test_constraint_updater.cc
+++ test/placement/test_constraint_updater.cc
@@ -4,49 +4,52 @@
 #include "../../src/placement/constraint_updater.h"
 #include "../../src/graphics/qimage_drawer.h"
 
-// TODO refactor test. Extract image comparison logic
-//
-TEST(Test_ConstraintUpdater, DrawWithConnectorShadows)
+class Test_ConstraintUpdater : public ::testing::Test
 {
-  int width = 512;
-  int height = 256;
-  auto drawer = std::make_shared<Graphics::QImageDrawer>(width, height);
-  ConstraintUpdater constraintUpdater(drawer, width, height);
-  constraintUpdater.setIsConnectorShadowEnabled(true);
+ protected:
+  virtual void SetUp()
+  {
+    int width = 512;
+    int height = 256;
+    drawer = std::make_shared<Graphics::QImageDrawer>(width, height);
+    constraintUpdater =
+        std::make_shared<ConstraintUpdater>(drawer, width, height);
+  }
 
-  constraintUpdater.clear();
+ public:
+  std::shared_ptr<Graphics::QImageDrawer> drawer;
+  std::shared_ptr<ConstraintUpdater> constraintUpdater;
 
-  Eigen::Vector2i anchorPosition(100, 50);
-  Eigen::Vector2i labelSize(62, 20);
-  Eigen::Vector2i lastAnchorPosition(200, 70);
-  Eigen::Vector2i lastLabelPosition(150, 120);
-  Eigen::Vector2i lastLabelSize(60, 40);
-  constraintUpdater.drawConstraintRegionFor(anchorPosition, labelSize,
-                                            lastAnchorPosition,
-                                            lastLabelPosition, lastLabelSize);
+  void drawConstraintRegion()
+  {
+    constraintUpdater->clear();
+
+    Eigen::Vector2i anchorPosition(100, 50);
+    Eigen::Vector2i labelSize(62, 20);
+    Eigen::Vector2i lastAnchorPosition(200, 70);
+    Eigen::Vector2i lastLabelPosition(150, 120);
+    Eigen::Vector2i lastLabelSize(60, 40);
+    constraintUpdater->drawConstraintRegionFor(
+        anchorPosition, labelSize, lastAnchorPosition, lastLabelPosition,
+        lastLabelSize);
+  }
+};
+
+TEST_F(Test_ConstraintUpdater, DrawWithConnectorShadows)
+{
+  constraintUpdater->setIsConnectorShadowEnabled(true);
+
+  drawConstraintRegion();
 
   compareImages("expected-constraints-with-connectors.png",
                 "constraints-with-connectors.png", drawer->image.get());
 }
 
-TEST(Test_ConstraintUpdater, DrawWithoutConnectorShadows)
+TEST_F(Test_ConstraintUpdater, DrawWithoutConnectorShadows)
 {
-  int width = 512;
-  int height = 256;
-  auto drawer = std::make_shared<Graphics::QImageDrawer>(width, height);
-  ConstraintUpdater constraintUpdater(drawer, width, height);
-  constraintUpdater.setIsConnectorShadowEnabled(false);
+  constraintUpdater->setIsConnectorShadowEnabled(false);
 
-  constraintUpdater.clear();
-
-  Eigen::Vector2i anchorPosition(100, 50);
-  Eigen::Vector2i labelSize(62, 20);
-  Eigen::Vector2i lastAnchorPosition(200, 70);
-  Eigen::Vector2i lastLabelPosition(150, 120);
-  Eigen::Vector2i lastLabelSize(60, 40);
-  constraintUpdater.drawConstraintRegionFor(anchorPosition, labelSize,
-                                            lastAnchorPosition,
-                                            lastLabelPosition, lastLabelSize);
+  drawConstraintRegion();
 
   compareImages("expected-constraints-without-connectors.png",
                 "constraints-without-connectors.png", drawer->image.get());
