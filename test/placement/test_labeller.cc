1f7498092aa2655296537539e01845bc58f26e64
christof <christof.sirk@gmail.com>
2016-04-17 14:33:39 +0200

    Convert Test_PlacementLabeller into a Fixture.

--- test/placement/test_labeller.cc
+++ test/placement/test_labeller.cc
@@ -7,76 +7,77 @@
 #include "../../src/utils/image_persister.h"
 #include "../../src/utils/path_helper.h"
 
-std::shared_ptr<Labels> createLabels()
+class Test_PlacementLabeller : public ::testing::Test
 {
-  auto labels = std::make_shared<Labels>();
-  labels->add(Label(1, "Shoulder", Eigen::Vector3f(0.174f, 0.55f, 0.034f)));
-  labels->add(Label(2, "Ellbow", Eigen::Vector3f(0.34f, 0.322f, -0.007f)));
-  labels->add(Label(3, "Wound", Eigen::Vector3f(0.262f, 0.422f, 0.058f),
-                    Eigen::Vector2i(128, 128)));
-  labels->add(Label(4, "Wound 2", Eigen::Vector3f(0.034f, 0.373f, 0.141f)));
+ protected:
+  void createLabeller()
+  {
+    const int width = 512;
+    const int height = 512;
 
-  return labels;
-}
+    cudaChannelFormatDesc floatChannelDesc =
+        cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
 
-std::shared_ptr<Placement::Labeller>
-createLabeller(std::shared_ptr<Labels> labels)
+    auto occupancyImage =
+        ImagePersister::loadR32F(absolutePathOfProjectRelativePath(
+            std::string("assets/tests/placement-labeller/occupancy.tiff")));
+    auto occupancyTextureMapper = std::make_shared<CudaArrayMapper<float>>(
+        width, height, occupancyImage, floatChannelDesc);
+
+    auto distanceTransformImage =
+        ImagePersister::loadR32F(absolutePathOfProjectRelativePath(std::string(
+            "assets/tests/placement-labeller/distanceTransform.tiff")));
+    auto distanceTransformTextureMapper =
+        std::make_shared<CudaArrayMapper<float>>(
+            width, height, distanceTransformImage, floatChannelDesc);
+
+    cudaChannelFormatDesc vector4ChannelDesc =
+        cudaCreateChannelDesc(32, 32, 32, 32, cudaChannelFormatKindFloat);
+    std::vector<Eigen::Vector4f> apolloniusImage(width * height,
+                                                 Eigen::Vector4f(0, 0, 0, 0));
+    auto apolloniusTextureMapper =
+        std::make_shared<CudaArrayMapper<Eigen::Vector4f>>(
+            width, height, apolloniusImage, vector4ChannelDesc);
+
+    cudaChannelFormatDesc byteChannelDesc =
+        cudaCreateChannelDesc(8, 0, 0, 0, cudaChannelFormatKindUnsigned);
+    std::vector<unsigned char> constraintImage(width * height, 0);
+    auto constraintTextureMapper =
+        std::make_shared<CudaArrayMapper<unsigned char>>(
+            width, height, constraintImage, byteChannelDesc);
+    auto drawer = std::make_shared<QImageDrawerWithUpdating>(
+        width, height, constraintTextureMapper);
+    auto constraintUpdater =
+        std::make_shared<ConstraintUpdater>(drawer, width, height);
+    auto persistentConstraintUpdater =
+        std::make_shared<PersistentConstraintUpdater>(constraintUpdater);
+
+    labeller = std::make_shared<Placement::Labeller>(labels);
+    labeller->initialize(occupancyTextureMapper, distanceTransformTextureMapper,
+                         apolloniusTextureMapper, constraintTextureMapper,
+                         persistentConstraintUpdater);
+    labeller->resize(1024, 1024);
+  }
+
+  virtual void SetUp()
+  {
+    labels = std::make_shared<Labels>();
+    labels->add(Label(1, "Shoulder", Eigen::Vector3f(0.174f, 0.55f, 0.034f)));
+    labels->add(Label(2, "Ellbow", Eigen::Vector3f(0.34f, 0.322f, -0.007f)));
+    labels->add(Label(3, "Wound", Eigen::Vector3f(0.262f, 0.422f, 0.058f),
+                      Eigen::Vector2i(128, 128)));
+    labels->add(Label(4, "Wound 2", Eigen::Vector3f(0.034f, 0.373f, 0.141f)));
+
+    createLabeller();
+  }
+
+ public:
+  std::shared_ptr<Labels> labels;
+  std::shared_ptr<Placement::Labeller> labeller;
+};
+
+TEST_F(Test_PlacementLabeller, UpdateCalculatesPositionsFromRealData)
 {
-  const int width = 512;
-  const int height = 512;
-
-  cudaChannelFormatDesc floatChannelDesc =
-      cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
-
-  auto occupancyImage =
-      ImagePersister::loadR32F(absolutePathOfProjectRelativePath(
-          std::string("assets/tests/placement-labeller/occupancy.tiff")));
-  auto occupancyTextureMapper = std::make_shared<CudaArrayMapper<float>>(
-      width, height, occupancyImage, floatChannelDesc);
-
-  auto distanceTransformImage =
-      ImagePersister::loadR32F(absolutePathOfProjectRelativePath(std::string(
-          "assets/tests/placement-labeller/distanceTransform.tiff")));
-  auto distanceTransformTextureMapper =
-      std::make_shared<CudaArrayMapper<float>>(
-          width, height, distanceTransformImage, floatChannelDesc);
-
-  cudaChannelFormatDesc vector4ChannelDesc =
-      cudaCreateChannelDesc(32, 32, 32, 32, cudaChannelFormatKindFloat);
-  std::vector<Eigen::Vector4f> apolloniusImage(width * height,
-                                               Eigen::Vector4f(0, 0, 0, 0));
-  auto apolloniusTextureMapper =
-      std::make_shared<CudaArrayMapper<Eigen::Vector4f>>(
-          width, height, apolloniusImage, vector4ChannelDesc);
-
-  cudaChannelFormatDesc byteChannelDesc =
-      cudaCreateChannelDesc(8, 0, 0, 0, cudaChannelFormatKindUnsigned);
-  std::vector<unsigned char> constraintImage(width * height, 0);
-  auto constraintTextureMapper =
-      std::make_shared<CudaArrayMapper<unsigned char>>(
-          width, height, constraintImage, byteChannelDesc);
-  auto drawer = std::make_shared<QImageDrawerWithUpdating>(
-      width, height, constraintTextureMapper);
-  auto constraintUpdater =
-      std::make_shared<ConstraintUpdater>(drawer, width, height);
-  auto persistentConstraintUpdater =
-      std::make_shared<PersistentConstraintUpdater>(constraintUpdater);
-
-  auto labeller = std::make_shared<Placement::Labeller>(labels);
-  labeller->initialize(occupancyTextureMapper, distanceTransformTextureMapper,
-                       apolloniusTextureMapper, constraintTextureMapper,
-                       persistentConstraintUpdater);
-  labeller->resize(1024, 1024);
-
-  return labeller;
-}
-
-TEST(Test_PlacementLabeller, UpdateCalculatesPositionsFromRealData)
-{
-
-  auto labels = createLabels();
-  auto labeller = createLabeller(labels);
-
   Placement::CostFunctionWeights weights;
   weights.labelShadowConstraint = 1e2f;
   weights.occupancy = 1.0f;
@@ -114,11 +115,9 @@ TEST(Test_PlacementLabeller, UpdateCalculatesPositionsFromRealData)
   }
 }
 
-TEST(Test_PlacementLabeller, UpdatesReturnsEmptyMapIfLabellerIsNotInitialized)
+TEST(Test_PlacementLabellerWithoutFixture, UpdatesReturnsEmptyMapIfLabellerIsNotInitialized)
 {
-  auto labels = createLabels();
-  auto labeller = std::make_shared<Placement::Labeller>(labels);
-
+  auto labeller = std::make_shared<Placement::Labeller>(std::make_shared<Labels>());
   LabellerFrameData frameData(0.02f, Eigen::Matrix4f::Identity(),
                               Eigen::Matrix4f::Identity());
 
