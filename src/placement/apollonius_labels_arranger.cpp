56fd05762d51796ca8b4c278f28fbde2c02c6970
christof <christof.sirk@gmail.com>
2016-04-29 19:39:33 +0200

    Extract apollonius into ApolloniusLabelsArranger.

---
+++ src/placement/apollonius_labels_arranger.cpp
@@ -0,0 +1,59 @@
+#include "./apollonius_labels_arranger.h"
+#include "../utils/cuda_array_provider.h"
+#include "./apollonius.h"
+
+namespace Placement
+{
+
+void ApolloniusLabelsArranger::initialize(
+    std::shared_ptr<CudaArrayProvider> distanceTransformTextureMapper,
+    std::shared_ptr<CudaArrayProvider> apolloniusTextureMapper)
+{
+  this->distanceTransformTextureMapper = distanceTransformTextureMapper;
+  this->apolloniusTextureMapper = apolloniusTextureMapper;
+}
+
+std::vector<Label> ApolloniusLabelsArranger::calculateOrder(
+    const LabellerFrameData &frameData, std::shared_ptr<LabelsContainer> labels)
+{
+  if (labels->count() == 1)
+    return std::vector<Label>{ labels->getLabels()[0] };
+
+  Eigen::Vector2i bufferSize(distanceTransformTextureMapper->getWidth(),
+                             distanceTransformTextureMapper->getHeight());
+  std::vector<Eigen::Vector4f> labelsSeed =
+      createLabelSeeds(bufferSize, frameData.viewProjection, labels);
+
+  Apollonius apollonius(distanceTransformTextureMapper, apolloniusTextureMapper,
+                        labelsSeed, labels->count());
+  apollonius.run();
+
+  std::vector<Label> result;
+  for (int id : apollonius.calculateOrdering())
+  {
+    result.push_back(labels->getById(id));
+  }
+
+  return result;
+}
+
+std::vector<Eigen::Vector4f> ApolloniusLabelsArranger::createLabelSeeds(
+    Eigen::Vector2i size, Eigen::Matrix4f viewProjection,
+    std::shared_ptr<LabelsContainer> labels)
+{
+  std::vector<Eigen::Vector4f> result;
+  for (auto &label : labels->getLabels())
+  {
+    Eigen::Vector4f pos =
+        viewProjection * Eigen::Vector4f(label.anchorPosition.x(),
+                                         label.anchorPosition.y(),
+                                         label.anchorPosition.z(), 1);
+    float x = (pos.x() / pos.w() * 0.5f + 0.5f) * size.x();
+    float y = (pos.y() / pos.w() * 0.5f + 0.5f) * size.y();
+    result.push_back(Eigen::Vector4f(label.id, x, y, 1));
+  }
+
+  return result;
+}
+
+}  // namespace Placement
