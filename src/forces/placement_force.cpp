7db95ee246d6cf9ef5f506653771981e26328fca
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2016-05-02 18:17:11 +0200

    - Ubuntu 16.04/G++5.3.1 changes - fixed wrong version of compute capability detection code

--- src/forces/placement_force.cpp
+++ src/forces/placement_force.cpp
@@ -12,8 +12,8 @@ PlacementForce::PlacementForce() : Force("Placement", 4.0f)
 Eigen::Vector2f PlacementForce::calculate(LabelState &label,
                                           std::vector<LabelState> &labels)
 {
-  if (isnan(label.placementPosition2D.x()) &&
-      isnan(label.placementPosition2D.y()))
+  if (std::isnan(label.placementPosition2D.x()) &&
+      std::isnan(label.placementPosition2D.y()))
     return Eigen::Vector2f(0, 0);
 
   Eigen::Vector2f direction = label.placementPosition2D - label.labelPosition2D;
