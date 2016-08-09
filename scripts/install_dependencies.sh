9f3872fe0e4e861ff326fbcfe4013e4f6014d5a8
christof <christof.sirk@gmail.com>
2016-08-09 21:42:33 +0200

    Update install_dependencies script.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,5 @@
 #Terminate on error
 set -e
 
-sudo apt-get install libassimp-dev doxygen graphviz freeglut3-dev libeigen3-dev  libinsighttoolkit4-dev libmagick++-dev -y
+sudo apt-get install libassimp-dev doxygen graphviz freeglut3-dev libeigen3-dev  libinsighttoolkit4-dev libmagick++-dev libboost-all-dev -y
 
