d193842dc7dcd873d23852d19d266c20f79c5f57
christof <christof.sirk@gmail.com>
2016-08-11 17:06:16 +0200

    Add libfftw3-dev to dependencies and reformat them.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,7 @@
 #Terminate on error
 set -e
 
-sudo apt-get install libassimp-dev doxygen graphviz freeglut3-dev libeigen3-dev  libinsighttoolkit4-dev libmagick++-dev libboost-all-dev -y
+sudo apt-get install libassimp-dev doxygen graphviz freeglut3-dev libeigen3-dev libboost-all-dev libmagick++-dev libfftw3-dev \
+  libinsighttoolkit4-dev libgdcm-tools vtk-dicom-tools python-vtkgdcm \
+  libavcodec-dev libswscale-dev libavformat-dev -y
 
