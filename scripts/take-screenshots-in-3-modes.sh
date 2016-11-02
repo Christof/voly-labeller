f6fdae78b548c422b84b71255e37736b4b655307
christof <christof.sirk@gmail.com>
2016-11-02 21:05:29 +0100

    Add script to take screenshot of given scene and camera position in different modes.

---
+++ scripts/take-screenshots-in-3-modes.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+#Terminate on error
+set -e
+
+script_directory="$(dirname "$0")"
+cd $script_directory
+cd ../build
+
+./voly-labeller $1 -s="$2_default"
+./voly-labeller $1 --disable-labelling --layers=1 -s="$2_no"
+./voly-labeller $1 --layers=1 --hard-constraints --apollonius -s="$2_decoret"
