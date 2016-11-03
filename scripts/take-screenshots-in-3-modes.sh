49f5cbcbd87a811af18eb7096aeb5518039c4935
christof <christof.sirk@gmail.com>
2016-11-03 17:40:12 +0100

    Also take screenshot of optimized result in script.

--- scripts/take-screenshots-in-3-modes.sh
+++ scripts/take-screenshots-in-3-modes.sh
@@ -8,5 +8,6 @@ cd $script_directory
 cd ../build
 
 ./voly-labeller $1 -s="$2_default"
+./voly-labeller $1 --optimize-on-idle -s="$2_optimized"
 ./voly-labeller $1 --disable-labelling --layers=1 -s="$2_no"
 ./voly-labeller $1 --layers=1 --hard-constraints --apollonius -s="$2_decoret"
