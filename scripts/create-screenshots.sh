837b869068d7bc96134755933570b39b6d7fa3c9
christof <christof.sirk@gmail.com>
2016-11-08 19:15:27 +0100

    Take even more screenshots.

--- scripts/create-screenshots.sh
+++ scripts/create-screenshots.sh
@@ -7,6 +7,11 @@ cd $script_directory
 ./take-screenshots-in-modes.sh ../scenes/heidelberg_schuss_cal45.xml "S1,S2,S3,S4,S5"
 ./take-screenshots-in-modes.sh ../scenes/jet-engine.xml "S0,S1,S2,S4"
 ./take-screenshots-in-modes.sh ../scenes/pedestrian.xml "S0,S1,S2,S3"
+./take-screenshots-in-modes.sh ../scenes/heidelberg_delikt_messer.xml "Screenshot"
+#./take-screenshots-in-modes.sh ../scenes/heidelberg_sturz2.xml "Screenshot"
+#./take-screenshots-in-modes.sh ../scenes/heidelberg_motorcycle.xml "Screenshot"
+./take-screenshots-in-modes.sh ../scenes/sponza.xml "S0,S1,S2,S3,S4"
+
 
 LUNG=""
 for ((i=0; i<=15; i++)); do
