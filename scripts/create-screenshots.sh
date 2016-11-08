1fafd868e0a14ea099019d3dbcddec868a1c38d8
christof <christof.sirk@gmail.com>
2016-11-08 19:10:09 +0100

    Add create-screenshots script to take many screenshots.

---
+++ scripts/create-screenshots.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+script_directory="$(dirname "$0")"
+cd $script_directory
+
+./take-screenshots-in-modes.sh ../scenes/heidelberg_motorcycle.xml "S0,S1,S2"
+./take-screenshots-in-modes.sh ../scenes/heidelberg_schuss_cal45.xml "S1,S2,S3,S4,S5"
+./take-screenshots-in-modes.sh ../scenes/jet-engine.xml "S0,S1,S2,S4"
+./take-screenshots-in-modes.sh ../scenes/pedestrian.xml "S0,S1,S2,S3"
+
+LUNG=""
+for ((i=0; i<=15; i++)); do
+  LUNG+="S$i,"
+done
+LUNG_ALL=${LUNG:0:${#LUNG}-1}
+./take-screenshots-in-modes.sh ../scenes/LIDC-IDRI_0469.xml "$LUNG_ALL"
