37e784e019f8aa9f3a4c5a23b6876c8e6ca1e6d8
christof <christof.sirk@gmail.com>
2016-11-08 18:15:06 +0100

    Add support to pass multiple camera positions separated by ',' to take-screenshots-in-modes.sh.

--- scripts/take-screenshots-in-modes.sh
+++ scripts/take-screenshots-in-modes.sh
@@ -7,8 +7,31 @@ script_directory="$(dirname "$0")"
 cd $script_directory
 cd ../build
 
-./voly-labeller $1 -s="$2_default"
-./voly-labeller $1 --internal-labelling -s="$2_internal"
-./voly-labeller $1 --optimize-on-idle -s="$2_optimized"
-./voly-labeller $1 --disable-labelling --layers=1 -s="$2_no"
-./voly-labeller $1 --layers=1 --hard-constraints --apollonius -s="$2_decoret"
+
+function getWithSuffix {
+  ALL=""
+  for i in $(echo $1 | tr "," "\n")
+  do
+    ALL+="$i"
+    ALL+="_"
+    ALL+="$2"
+    ALL+=","
+  done
+
+  PARAMS=${ALL:0:${#ALL}-1}
+}
+
+getWithSuffix $2 "default"
+./voly-labeller $1 -s=$PARAMS
+
+getWithSuffix $2 "internal"
+./voly-labeller $1 --internal-labelling -s="$PARAMS"
+
+getWithSuffix $2 "optimized"
+./voly-labeller $1 --optimize-on-idle -s="$PARAMS"
+
+getWithSuffix $2 "no"
+./voly-labeller $1 --disable-labelling --layers=1 -s="$PARAMS"
+
+getWithSuffix $2 "decoret"
+./voly-labeller $1 --layers=1 --hard-constraints --apollonius -s="$PARAMS"
