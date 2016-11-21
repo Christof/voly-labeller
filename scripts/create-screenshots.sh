272999bb95d70c2c2c4928c5b68460439be87b99
christof <christof.sirk@gmail.com>
2016-11-21 11:18:59 +0100

    Use new delikt messer scene in scripts.

--- scripts/create-screenshots.sh
+++ scripts/create-screenshots.sh
@@ -7,13 +7,12 @@ cd $script_directory
 ./take-screenshots-in-modes.sh ../scenes/heidelberg_schuss_cal45.xml "S1,S2,S3,S4,S5"
 ./take-screenshots-in-modes.sh ../scenes/jet-engine.xml "S0,S1,S2,S4"
 ./take-screenshots-in-modes.sh ../scenes/pedestrian.xml "S0,S1,S2,S3"
-./take-screenshots-in-modes.sh ../scenes/heidelberg_delikt_messer.xml "Screenshot"
+./take-screenshots-in-modes.sh ../scenes/heidelberg_delikt_messer2.xml "S0,S1,S2,S3,S4,S5,S6,S7,S8,S9"
 ./take-screenshots-in-modes.sh ../scenes/heidelberg_sturz2.xml "S0,S1,S2,S3,S4,s5"
 ./take-screenshots-in-modes.sh ../scenes/grch.xml "S0,S1,S2"
 ./take-screenshots-in-modes.sh ../scenes/sponza.xml "S0,S1,S2,S3,S4"
 ./take-screenshots-in-modes.sh ../scenes/plane.xml "S0,S1,S2,S3"
 ./take-screenshots-in-modes.sh ../scenes/human.xml "P0,P1,P2,P3,P4,S0"
-
 
 LUNG=""
 for ((i=0; i<=15; i++)); do
--- scripts/create-videos.sh
+++ scripts/create-videos.sh
@@ -9,7 +9,7 @@ echo $PARAMETERS
 ./voly-labeller ../scenes/heidelberg_schuss_cal45.xml "$PARAMETERS" --video="P0,P1,P2,P3,P4,P5,P6,P7,S2,S3,S4,S5"
 ./voly-labeller ../scenes/jet-engine.xml "$PARAMETERS" "S0,S1,S2,S4"
 ./voly-labeller ../scenes/pedestrian.xml "$PARAMETERS" --video="S0,S1,S2,S3"
-./voly-labeller ../scenes/heidelberg_delikt_messer.xml "$PARAMETERS" --video="Screenshot"
+./voly-labeller ../scenes/heidelberg_delikt_messer2.xml "$PARAMETERS" --video="S0,S1,S2,S3_7,S4,S5,S6,S7_7,S8,S9"
 ./voly-labeller ../scenes/heidelberg_sturz2.xml "$PARAMETERS" --video="S0,S1,S2,S3,S4,S5"
 ./voly-labeller ../scenes/grch.xml "$PARAMETERS" --video="S0,S1,S2_6,S3"
 ./voly-labeller ../scenes/sponza.xml "$PARAMETERS" "S0,S1,S2,S3,S4"
