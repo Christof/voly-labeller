f067bb0b25e8715ecca22c91aa81e31a51491752
christof <christof.sirk@gmail.com>
2016-11-22 21:14:00 +0100

    Fix passing of parameter create-videos.sh. Also time how long it takes.

--- scripts/create-videos.sh
+++ scripts/create-videos.sh
@@ -3,22 +3,32 @@
 script_directory="$(dirname "$0")"
 cd $script_directory/../build
 
-PARAMETERS="$1 --offline"
-echo $PARAMETERS
-./voly-labeller ../scenes/heidelberg_motorcycle.xml "$PARAMETERS" --video="S0,S1,S2"
-./voly-labeller ../scenes/heidelberg_schuss_cal45.xml "$PARAMETERS" --video="P0,P1,P2,P3,P4,P5,P6,P7,S2,S3,S4,S5"
-./voly-labeller ../scenes/jet-engine.xml "$PARAMETERS" "S0,S1,S2,S4"
-./voly-labeller ../scenes/pedestrian.xml "$PARAMETERS" --video="S0,S1,S2,S3"
-./voly-labeller ../scenes/heidelberg_delikt_messer2.xml "$PARAMETERS" --video="S0,S1,S2,S3_7,S4,S5,S6,S7_7,S8,S9"
-./voly-labeller ../scenes/heidelberg_sturz2.xml "$PARAMETERS" --video="S0,S1,S2,S3,S4,S5"
-./voly-labeller ../scenes/grch.xml "$PARAMETERS" --video="S0,S1,S2_6,S3"
-./voly-labeller ../scenes/sponza.xml "$PARAMETERS" "S0,S1,S2,S3,S4"
-./voly-labeller ../scenes/plane.xml "$PARAMETERS" --video="S0,S1,S2,S3"
-./voly-labeller ../scenes/human.xml "$PARAMETERS" --video="P0,P1,P2"
+PARAMETERS="--offline"
+time ./voly-labeller ../scenes/heidelberg_motorcycle.xml $1 $PARAMETERS\
+  --video="S0,S1,S2"
+time ./voly-labeller ../scenes/heidelberg_schuss_cal45.xml $1 $PARAMETERS\
+  --video="P0,P1,P2,P3,P4,P5,P6,P7,S2,S3,S4,S5"
+time ./voly-labeller ../scenes/jet-engine.xml $1 $PARAMETERS\
+  --video="S0,S1,S2,S4"
+time ./voly-labeller ../scenes/pedestrian.xml $1 $PARAMETERS\
+  --video="S0,S1,S2,S3"
+time ./voly-labeller ../scenes/heidelberg_delikt_messer2.xml $1 $PARAMETERS\
+  --video="S0,S1,S2,S3_7,S4,S5,S6,S7_7,S8,S9"
+time ./voly-labeller ../scenes/heidelberg_sturz2.xml $1 $PARAMETERS\
+  --video="S0,S1,S2,S3,S4,S5"
+time ./voly-labeller ../scenes/grch.xml $1 $PARAMETERS\
+  --video="S0,S1,S2_6,S3"
+time ./voly-labeller ../scenes/sponza.xml $1 $PARAMETERS\
+  --video="S0,S1,S2,S3,S4"
+time ./voly-labeller ../scenes/plane.xml $1 $PARAMETERS\
+  --video="S0,S1,S2,S3"
+time ./voly-labeller ../scenes/human.xml $1 $PARAMETERS\
+  --video="P0,P1,P2"
 
 LUNG=""
 for ((i=0; i<=15; i++)); do
   LUNG+="S$i,"
 done
 LUNG_ALL=${LUNG:0:${#LUNG}-1}
-./voly-labeller ../scenes/LIDC-IDRI_0469.xml $PARAMETERS --video="$LUNG_ALL"
+time ./voly-labeller ../scenes/LIDC-IDRI_0469.xml $1 $PARAMETERS\
+  --video="$LUNG_ALL"
