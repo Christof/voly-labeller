027dfbae4855d0f942e42b27370f7871c35625d8
christof <christof.sirk@gmail.com>
2017-03-13 17:22:18 +0100

    Add a transfer function for parser testing and extend the existing test.

--- test/utils/test_gradient_utils.cc
+++ test/utils/test_gradient_utils.cc
@@ -5,9 +5,22 @@
 TEST(Test_GradientUtils, LoadFromFile)
 {
   QGradient gradient =
-      GradientUtils::loadGradient("../assets/transferfunctions/scapula1.gra");
+      GradientUtils::loadGradient("../assets/transferfunctions/test.gra");
 
-  EXPECT_EQ(6, gradient.stops().size());
+  EXPECT_EQ(3, gradient.stops().size());
+
+  QGradientStop gradient0(0.298828, QColor(0, 0, 0, 0));
+  EXPECT_FLOAT_EQ(gradient0.first, gradient.stops()[0].first);
+  EXPECT_EQ(gradient0.second, gradient.stops()[0].second);
+
+  QGradientStop gradient1(0.311035, QColor(0, 100, 0, 76));
+  EXPECT_FLOAT_EQ(gradient1.first, gradient.stops()[1].first);
+  EXPECT_EQ(gradient1.second, gradient.stops()[1].second);
+
+  QGradientStop gradient2(0.37207, QColor(30, 10, 40, 255));
+  EXPECT_FLOAT_EQ(gradient2.first, gradient.stops()[2].first);
+  EXPECT_EQ(gradient2.second, gradient.stops()[2].second);
+
   EXPECT_EQ(QGradient::Type::LinearGradient, gradient.type());
 }
 
